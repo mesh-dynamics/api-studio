96f870c0be58b8b9be3c1ed696a459092c8dcd06
Venky Ganti <vganti@live.com>
2019-03-21 16:25:13 -0700

    Python 3 fixes.

--- moviebook/fetch_servicenames.py
+++ moviebook/fetch_servicenames.py
@@ -5,7 +5,7 @@ import os
 
 data_stream = open('moviebook/moviebook.yaml', 'r')
 dataMap = yaml.load_all(data_stream, Loader=yaml.SafeLoader)
-with open("moviebook/services.yaml", "wb") as f:
+with open("moviebook/services.yaml", "w") as f:
     f.write("services:\n")
     for config in dataMap:
         if config['kind'] == 'Service':
