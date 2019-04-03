5720984369405221d9e63abf042b6c62d63b9c3c
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-04-03 19:36:48 +0530

    Test cases for body in response

--- server/src/test/java/com/cube/core/ResponseComparatorTest.java
+++ server/src/test/java/com/cube/core/ResponseComparatorTest.java
@@ -37,7 +37,7 @@ public class ResponseComparatorTest {
     static Config config;
     JSONObject object;
     static ObjectMapper mapper;
-    static String[] id;
+    static String[] idList;
 
     /**
      * @throws java.lang.Exception
@@ -57,7 +57,7 @@ public class ResponseComparatorTest {
             "restwrapjdbce6acccf2-cf09-499f-a7dc-d6b4a11381ba",
             "restwrapjdbc6467c26f-2b7e-4441-a1f2-3f6d6707e4db"
         };
-        id = idArr;
+        idList = idArr;
         mapper = config.jsonmapper;
         mapper.registerModule(new JavaTimeModule());
     }
@@ -96,36 +96,6 @@ public class ResponseComparatorTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void compareTest(JSONObject testData, Response response1, Response response2) throws JsonProcessingException, JSONException{
-        JSONArray rules = testData.getJSONArray("rules");
-        String expected = testData.get("output").toString();
-        System.out.println(mapper.writeValueAsString(response1));
-        System.out.println(mapper.writeValueAsString(response2));
-        CompareTemplate template = new CompareTemplate();
-
-        for (int i = 0; i < rules.length(); i++) {
-            JSONObject ruleObj = rules.getJSONObject(i);
-            String path = ruleObj.getString("path");
-            DataType dataType = DataType.valueOf(ruleObj.getString("dataType"));
-            PresenceType presenceType = PresenceType.valueOf(ruleObj.getString("presenceType"));
-            ComparisonType comparisonType = ComparisonType.valueOf(ruleObj.getString("comparisonType"));
-            String customization = ruleObj.getString("customization");
-            TemplateEntry rule;
-            if (customization.isEmpty()) {
-                rule = new TemplateEntry(path, dataType, presenceType, comparisonType);
-            } else {
-                rule = new TemplateEntry(path, dataType, presenceType, comparisonType, Optional.of(customization));
-            }
-            template.addRule(rule);
-        }
-
-        TemplatedResponseComparator comparator = new TemplatedResponseComparator(template, mapper);
-        Match m = comparator.compare(response1, response2);
-
-        String mjson = config.jsonmapper.writeValueAsString(m);
-        JSONAssert.assertEquals(expected, mjson, false);
     }
 
     /**
@@ -183,30 +153,100 @@ public class ResponseComparatorTest {
      * @throws JSONException
      */
     @Test
-    @DisplayName("Default comparison test")
-    final void defaultComparisonTest() throws IOException, JSONException {
-        ObjectMapper mapper = config.jsonmapper;
-        mapper.registerModule(new JavaTimeModule());
-        Optional<Response> response1 = config.rrstore.getResponse(id[1]);
+    @DisplayName("Same Response body test: Negative")
+    final void sameResponseBodyNegativeTest() throws IOException, JSONException {
+        JSONObject testData = object.getJSONObject("sameResponseBodyNegative");
+        String res1 = testData.get("res1").toString();
+        Response response1 = config.rrstore.getResponse(res1).get();
+        JSONObject body = new JSONObject(response1.body);
+        body.put("year", 1000);
+        body.put("type", "softcopy");
+        body.put("pages", 3.14);
+        Response response2 = new Response(response1.reqid, response1.status, response1.meta, response1.hdrs,
+            body.toString(), response1.collection, response1.timestamp, response1.rrtype, response1.customerid, response1.app);
+        compareTest(testData, response1, response2);
+    }
+
+    /**
+     * Test method for {@link com.cube.core.TemplatedResponseComparator#compare(Response, Response)} .
+     * @throws JsonProcessingException
+     * @throws JSONException
+     */
+    @Test
+    @DisplayName("Different response Body test")
+    final void differentResponseBodyTest() throws IOException, JSONException {
+        JSONObject testData = object.getJSONObject("differentResponseBody");
+        String res1 = testData.get("res1").toString();
+        String res2 = testData.get("res2").toString();
+        Response response1 = config.rrstore.getResponse(res1).get();
+        Response response2 = config.rrstore.getResponse(res2).get();
+        compareTest(testData, response1, response2);
+    }
+
+    private void compareTest(JSONObject testData, Response response1, Response response2) throws JsonProcessingException, JSONException{
+        JSONArray rules = testData.getJSONArray("rules");
+        String expected = testData.get("output").toString();
         System.out.println(mapper.writeValueAsString(response1));
-        Optional<Response> response2 = config.rrstore.getResponse(id[2]);
         System.out.println(mapper.writeValueAsString(response2));
         CompareTemplate template = new CompareTemplate();
-        template.addRule(new TemplateEntry("/status", DataType.Str, PresenceType.Required, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(PATHPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(QPARAMPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(FPARAMPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(RRTYPEPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(CUSTOMERIDPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(APPPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(REQIDPATH, DataType.Str, PresenceType.Optional, ComparisonType.EqualOptional));
-        template.addRule(new TemplateEntry(COLLECTIONPATH, CompareTemplate.DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(METAPATH + "/" + SERVICEFIELD, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
-        template.addRule(new TemplateEntry(HDRPATH+"/"+Config.DEFAULT_TRACE_FIELD, DataType.Str, PresenceType.Optional, ComparisonType.EqualOptional));
+        for (int i = 0; i < rules.length(); i++) {
+            JSONObject ruleObj = rules.getJSONObject(i);
+            String path = ruleObj.getString("path");
+            DataType dataType = DataType.valueOf(ruleObj.getString("dataType"));
+            PresenceType presenceType = PresenceType.valueOf(ruleObj.getString("presenceType"));
+            ComparisonType comparisonType = ComparisonType.valueOf(ruleObj.getString("comparisonType"));
+            String customization = ruleObj.getString("customization");
+            TemplateEntry rule;
+            if (customization.isEmpty()) {
+                rule = new TemplateEntry(path, dataType, presenceType, comparisonType);
+            } else {
+                rule = new TemplateEntry(path, dataType, presenceType, comparisonType, Optional.of(customization));
+            }
+            template.addRule(rule);
+        }
+
         TemplatedResponseComparator comparator = new TemplatedResponseComparator(template, mapper);
-        Match m = comparator.compare(response1.get(), response2.get());
+        Match m = comparator.compare(response1, response2);
+
         String mjson = config.jsonmapper.writeValueAsString(m);
-        System.out.println(mjson);
+        JSONAssert.assertEquals(expected, mjson, false);
+    }
+
+    /**
+     * Test method for {@link com.cube.core.TemplatedResponseComparator#compare(Response, Response)} .
+     * @throws JsonProcessingException
+     * @throws JSONException
+     */
+    @Test
+    @DisplayName("Default comparison test")
+    final void defaultComparisonTest() throws IOException, JSONException {
+
+        for (String id: idList){
+            Optional<Response> response = config.rrstore.getResponse(id);
+            System.out.println(response.get().body);
+        }
+//        ObjectMapper mapper = config.jsonmapper;
+//        mapper.registerModule(new JavaTimeModule());
+//        Optional<Response> response1 = config.rrstore.getResponse(id[1]);
+//        System.out.println(mapper.writeValueAsString(response1));
+//        Optional<Response> response2 = config.rrstore.getResponse(id[2]);
+//        System.out.println(mapper.writeValueAsString(response2));
+//        CompareTemplate template = new CompareTemplate();
+//        template.addRule(new TemplateEntry("/status", DataType.Str, PresenceType.Required, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(PATHPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(QPARAMPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(FPARAMPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(RRTYPEPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(CUSTOMERIDPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(APPPATH, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(REQIDPATH, DataType.Str, PresenceType.Optional, ComparisonType.EqualOptional));
+//        template.addRule(new TemplateEntry(COLLECTIONPATH, CompareTemplate.DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(METAPATH + "/" + SERVICEFIELD, DataType.Str, PresenceType.Optional, ComparisonType.Equal));
+//        template.addRule(new TemplateEntry(HDRPATH+"/"+Config.DEFAULT_TRACE_FIELD, DataType.Str, PresenceType.Optional, ComparisonType.EqualOptional));
+//        TemplatedResponseComparator comparator = new TemplatedResponseComparator(template, mapper);
+//        Match m = comparator.compare(response1.get(), response2.get());
+//        String mjson = config.jsonmapper.writeValueAsString(m);
+//        System.out.println(mjson);
     }
 
 }
