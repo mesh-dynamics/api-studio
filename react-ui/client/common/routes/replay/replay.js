036090bbd89c0b1340d3962da85fd880938a825c
dhiraj_bh <dhiraj@beehyv.com>
2019-04-10 09:46:42 +0530

    Result Tabs added

--- react-ui/client/common/routes/replay/replay.js
+++ react-ui/client/common/routes/replay/replay.js
@@ -6,6 +6,7 @@ import ServiceGraph from './ServiceGraph';
 import ReplayAttribute from "./ReplayAttribute";
 import "./ReplayAttribute.css";
 import Results from "./Results";
+import PiChart from "../../components/Graph/PiChart";
 
 class replay extends Component {
   constructor (props) {
@@ -44,9 +45,79 @@ class replay extends Component {
   }
 
   render () {
-    const { panelVisible } = this.state
+    const { panelVisible } = this.state;
+    const { cube } = this.props;
     const onHide = e => this.setState({panelVisible: !panelVisible})
     const { user, match, nctData, neList, version } = this.props;
+    let result = '';
+      let rightPane = '';
+    if (cube.report) {
+        result = (
+            <div className="result-container">
+                <Tabs defaultActiveKey="result" id="uncontrolled-tab-example">
+                    <Tab eventKey="result" title="Test Results: Detailed view">
+                        <Results/>
+                    </Tab>
+                    <Tab eventKey="analysis" title="Analysis">
+                        <div>In Prog</div>
+                    </Tab>
+                </Tabs>
+            </div>
+        );
+
+        rightPane = (
+            <div>
+                <div>
+                    <div className="right-head">
+                        Test Results Overview
+                    </div>
+                    <PiChart/>
+                </div>
+
+                <div>
+                    <div className="right-head">
+                        Test Configuration
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            Test Configuration
+                        </div>
+                        <div className="col-md-8 pull-right">
+                            HR-Primary-2019-3-RC-18
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            Collection
+                        </div>
+                        <div className="col-md-8 pull-right">
+                            {cube.selectedTestId}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+
+                        </div>
+                        <div className="col-md-8 pull-right">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        result = '';
+        rightPane = (
+            <div>
+                <div className="right-head">
+                    Replay
+                </div>
+                <input type="radio" name="replayType" value="male" checked /> <span className="rep">Replay All</span><br/>
+                <input type="radio" name="replayType" value="female" /> <span className="rep">Filter by Paths</span><br/>
+                <input type="radio" name="replayType" value="other" /> <span className="rep">Filter for Specific Requests (max 25)</span>
+            </div>
+        );
+    }
     return (
       <div>
 
@@ -63,25 +134,10 @@ class replay extends Component {
                 </div>
 
                 <div className="col-sm-3 border-bottom">
-                    <div className="right-head">
-                        Replay
-                    </div>
-                    <input type="radio" name="replayType" value="male" checked /> <span className="rep">Replay All</span><br/>
-                    <input type="radio" name="replayType" value="female" /> <span className="rep">Filter by Paths</span><br/>
-                    <input type="radio" name="replayType" value="other" /> <span className="rep">Filter for Specific Requests (max 25)</span>
+                    {rightPane}
                 </div>
             </Row>
-
-            <div className="result-container">
-                <Tabs defaultActiveKey="result" id="uncontrolled-tab-example">
-                    <Tab eventKey="result" title="Result">
-                        <Results/>
-                    </Tab>
-                    <Tab eventKey="analysis" title="Analysis">
-                        <div>In Prog</div>
-                    </Tab>
-                </Tabs>
-            </div>
+            {result}
         </div>
         
       </div>
@@ -94,9 +150,9 @@ class replay extends Component {
 
 function mapStateToProps(state) {
   const { user } = state.authentication;
-
+  const cube = state.cube;
   return {
-    user,
+    user, cube
   }
 }
 
