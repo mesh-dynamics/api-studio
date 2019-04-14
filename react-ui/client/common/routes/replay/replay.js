4428bfb7723589e732a060c3412b43ed9d41b1ca
dhiraj_bh <dhiraj@beehyv.com>
2019-04-15 03:21:32 +0530

    Pie changes

--- react-ui/client/common/routes/replay/replay.js
+++ react-ui/client/common/routes/replay/replay.js
@@ -9,151 +9,157 @@ import Results from "./Results";
 import PiChart from "../../components/Graph/PiChart";
 
 class replay extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      panelVisible: true,
+    constructor (props) {
+        super(props)
+        this.state = {
+            panelVisible: true,
+        };
+        this.pieRef = React.createRef();
+        this.handleChangeNeDropDown = this.handleChangeNeDropDown.bind(this);
+        this.refreshButtonHandler = this.refreshButtonHandler.bind(this);
     }
-    this.handleChangeNeDropDown = this.handleChangeNeDropDown.bind(this);
-    this.refreshButtonHandler = this.refreshButtonHandler.bind(this);
-  }
-  
-  componentDidMount () {
-    const { dispatch, user, neList } = this.props;
-    //dispatch(nodMonActions.getNeList(neList, user.user));
-  }
 
-
-  componentWillUnmount() {
-    const { dispatch, user, nctData } = this.props;
-    //dispatch(nodMonActions.stopNctData(user.user, nctData.socket));
-  }
-
-  handleChangeNeDropDown(e){
-    const { user, match, history, dispatch, nctData } = this.props;
-    if (e && e.label) {
-      let neIp = e.label.props.children[0];
-      history.push(`${match.url}/${neIp}`);
+    componentDidMount () {
+        const { dispatch, user, neList } = this.props;
+        //dispatch(nodMonActions.getNeList(neList, user.user));
     }
-  } 
 
-  
-  refreshButtonHandler() {
-    //console.log('refresh clicked');
-    //const { dispatch, user, neList } = this.props;       
-    //dispatch(nodMonActions.getNeList(neList, user.user, 'REFRESH'));    
-  }
 
-  render () {
-    const { panelVisible } = this.state;
-    const { cube } = this.props;
-    const onHide = e => this.setState({panelVisible: !panelVisible})
-    const { user, match, nctData, neList, version } = this.props;
-    let result = '';
-      let rightPane = '';
-    if (cube.report) {
-        result = (
-            <div className="result-container">
-                <Tabs defaultActiveKey="result" id="uncontrolled-tab-example">
-                    <Tab eventKey="result" title="Test Results: Detailed view">
-                        <Results/>
-                    </Tab>
-                    <Tab eventKey="analysis" title="Analysis">
-                        <div>In Prog</div>
-                    </Tab>
-                </Tabs>
-            </div>
-        );
+    componentWillUnmount() {
+        const { dispatch, user, nctData } = this.props;
+        //dispatch(nodMonActions.stopNctData(user.user, nctData.socket));
+    }
 
-        rightPane = (
-            <div>
-                <div>
-                    <div className="right-head">
-                        Test Results Overview
-                    </div>
-                    <PiChart/>
+    handleChangeNeDropDown(e){
+        const { user, match, history, dispatch, nctData } = this.props;
+        if (e && e.label) {
+            let neIp = e.label.props.children[0];
+            history.push(`${match.url}/${neIp}`);
+        }
+    }
+
+
+    refreshButtonHandler() {
+
+    }
+
+    render () {
+        const { panelVisible } = this.state;
+        const { cube } = this.props;
+        const onHide = e => this.setState({panelVisible: !panelVisible})
+        const { user, match, nctData, neList, version } = this.props;
+        let result = '';
+        let rightPane = '';
+        let pieData = [];
+        let width = this.pieRef && this.pieRef.current ? this.pieRef.current.offsetWidth : 0;
+        if (cube.analysis) {
+            pieData.push({name: 'success', value: cube.analysis.respmatched + cube.analysis.resppartiallymatched});
+            pieData.push({name: 'error', value: cube.analysis.respnotmatched});
+            pieData.push({name: 'incomplete', value: cube.analysis.reqcnt - (cube.analysis.respmatched + cube.analysis.resppartiallymatched + cube.analysis.respnotmatched)});
+
+            result = (
+                <div className="result-container">
+                    <Tabs defaultActiveKey="result" id="uncontrolled-tab-example">
+                        <Tab eventKey="result" title="Test Results: Detailed view">
+                            <Results/>
+                        </Tab>
+                        <Tab eventKey="analysis" title="Analysis">
+                            <div>In Prog</div>
+                        </Tab>
+                    </Tabs>
                 </div>
+            );
 
+            rightPane = (
                 <div>
-                    <div className="right-head">
-                        Test Configuration
+                    <div>
+                        <div className="right-head">
+                            Test Results Overview
+                        </div>
+                        <PiChart width={width} data={pieData}/>
                     </div>
-                    <div className="row">
-                        <div className="col-md-4">
+
+                    <div>
+                        <div className="right-head">
                             Test Configuration
                         </div>
-                        <div className="col-md-8 pull-right">
-                            HR-Primary-2019-3-RC-18
+                        <div className="row">
+                            <div className="col-md-4">
+                                Test Configuration
+                            </div>
+                            <div className="col-md-8 pull-right">
+                                HR-Primary-2019-3-RC-18
+                            </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-                            Collection
+                        <div className="row">
+                            <div className="col-md-4">
+                                Collection
+                            </div>
+                            <div className="col-md-8 pull-right">
+                                {cube.selectedTestId}
+                            </div>
                         </div>
-                        <div className="col-md-8 pull-right">
-                            {cube.selectedTestId}
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4">
-
-                        </div>
-                        <div className="col-md-8 pull-right">
-
+                        <div className="row">
+                            <div className="col-md-4">
+                                Total Requests
+                            </div>
+                            <div className="col-md-8 pull-right">
+                                {cube.analysis.reqcnt}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
-    } else {
-        result = '';
-        rightPane = (
+            );
+        } else {
+            pieData = [];
+            result = '';
+            rightPane = (
+                <div>
+                    <div className="right-head">
+                        Replay
+                    </div>
+                    <input type="radio" name="replayType" value="male" checked /> <span className="rep">Replay All</span><br/>
+                    <input type="radio" name="replayType" value="female" /> <span className="rep">Filter by Paths</span><br/>
+                    <input type="radio" name="replayType" value="other" /> <span className="rep">Filter for Specific Requests (max 25)</span>
+                </div>
+            );
+        }
+        return (
             <div>
-                <div className="right-head">
-                    Replay
+
+                <Clearfix />
+                <ReplayAttribute />
+                <Clearfix />
+                <div className="replay-container">
+                    <Row>
+                        {/*Service Graph: */}
+                        <div className="col-sm-8 border-bottom">
+                            {
+                                <ServiceGraph />
+                            }
+                        </div>
+
+                        <div className="col-sm-4 border-bottom no-padding" ref={this.pieRef}>
+                            {rightPane}
+                        </div>
+                    </Row>
+                    {result}
                 </div>
-                <input type="radio" name="replayType" value="male" checked /> <span className="rep">Replay All</span><br/>
-                <input type="radio" name="replayType" value="female" /> <span className="rep">Filter by Paths</span><br/>
-                <input type="radio" name="replayType" value="other" /> <span className="rep">Filter for Specific Requests (max 25)</span>
+
             </div>
-        );
+        )
     }
-    return (
-      <div>
-
-        <Clearfix />
-        <ReplayAttribute />
-        <Clearfix />
-        <div className="replay-container">
-            <Row>
-                {/*Service Graph: */}
-                <div className="col-sm-9 border-bottom">
-                {
-                    <ServiceGraph />
-                }
-                </div>
-
-                <div className="col-sm-3 border-bottom">
-                    {rightPane}
-                </div>
-            </Row>
-            {result}
-        </div>
-        
-      </div>
-    )
-  }
 }
 
 
 
 
 function mapStateToProps(state) {
-  const { user } = state.authentication;
-  const cube = state.cube;
-  return {
-    user, cube
-  }
+    const { user } = state.authentication;
+    const cube = state.cube;
+    return {
+        user, cube
+    }
 }
 
 const connectedReplay = connect(mapStateToProps)(replay);
