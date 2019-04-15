af8f6b97c6d30033f1eca6a8fb4d7e504f35afb8
prasad-cubeio <44723610+prasad-cubeio@users.noreply.github.com>
2019-04-15 17:09:31 +0530

    Update ServiceGraph.js

--- react-ui/client/common/routes/replay/ServiceGraph.js
+++ react-ui/client/common/routes/replay/ServiceGraph.js
@@ -160,17 +160,18 @@ class ServiceGraph extends Component {
         const { cube } = this.props;
         const recordedResponse = [{"actors_lastnames":["HARRIS","WILLIS","TEMPLE"],"display_actors":["DAN HARRIS","HUMPHREY WILLIS","BURT TEMPLE"],"film_id":851,"title":"STRAIGHT HOURS","actors_firstnames":["DAN","HUMPHREY","BURT"],"film_counts":[28,26,23],"timestamp":1641491700530174,"book_info":{"reviews":[{"reviewer":"Reviewer1","text":"An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!"},{"reviewer":"Reviewer2","text":"Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare."}],"id":"851"}}];
         const replayRes1 = [{"display_actors":["HARRIS,DAN","WILLIS,HUMPHREY","TEMPLE,BURT"],"film_id":851,"title":"STRAIGHT HOURS","actors_firstnames":["DAN","BURT","HUMPHREY"],"film_counts":["28","23","26"],"timestamp":27523407007561}];
-        const diff1 = 'DIFF :: {"op":"remove","path":"/body/0/actors_lastnames","value":["HARRIS","WILLIS","TEMPLE"],"resolution":"OK_Optional"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/display_actors/0","value":"HARRIS,DAN","fromValue":"DAN HARRIS","resolution":"OK_OptionalMismatch"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/display_actors/1","value":"WILLIS,HUMPHREY","fromValue":"HUMPHREY WILLIS","resolution":"OK_OptionalMismatch"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/display_actors/2","value":"TEMPLE,BURT","fromValue":"BURT TEMPLE","resolution":"OK_OptionalMismatch"}\n' +
-            'DIFF :: {"op":"add","path":"/body/0/actors_firstnames/1","value":"BURT","resolution":"OK_OtherValInvalid"}\n' +
-            'DIFF :: {"op":"remove","path":"/body/0/actors_firstnames/3","value":"BURT","resolution":"OK"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/film_counts/0","value":"28","fromValue":28,"resolution":"ERR_ValTypeMismatch"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/film_counts/1","value":"23","fromValue":26,"resolution":"ERR_ValTypeMismatch"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/film_counts/2","value":"26","fromValue":23,"resolution":"ERR_ValTypeMismatch"}\n' +
-            'DIFF :: {"op":"replace","path":"/body/0/timestamp","value":27523407007561,"fromValue":1641491700530174,"resolution":"OK_OptionalMismatch"}\n' +
-            'DIFF :: {"op":"remove","path":"/body/0/book_info","value":{"reviews":[{"reviewer":"Reviewer1","text":"An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!"},{"reviewer":"Reviewer2","text":"Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare."}],"id":"851"},"resolution":"OK"}';
+        const diff1 = 'DIFFS: \n' +
+            '{"op":"remove","path":"/body/0/actors_lastnames","value":["HARRIS","WILLIS","TEMPLE"],"resolution":"OK_Optional"}\n' +
+            '{"op":"replace","path":"/body/0/display_actors/0","value":"HARRIS,DAN","fromValue":"DAN HARRIS","resolution":"OK_OptionalMismatch"}\n' +
+            '{"op":"replace","path":"/body/0/display_actors/1","value":"WILLIS,HUMPHREY","fromValue":"HUMPHREY WILLIS","resolution":"OK_OptionalMismatch"}\n' +
+            '{"op":"replace","path":"/body/0/display_actors/2","value":"TEMPLE,BURT","fromValue":"BURT TEMPLE","resolution":"OK_OptionalMismatch"}\n' +
+            '{"op":"add","path":"/body/0/actors_firstnames/1","value":"BURT","resolution":"OK_OtherValInvalid"}\n' +
+            '{"op":"remove","path":"/body/0/actors_firstnames/3","value":"BURT","resolution":"OK"}\n' +
+            '{"op":"replace","path":"/body/0/film_counts/0","value":"28","fromValue":28,"resolution":"ERR_ValTypeMismatch"}\n' +
+            '{"op":"replace","path":"/body/0/film_counts/1","value":"23","fromValue":26,"resolution":"ERR_ValTypeMismatch"}\n' +
+            '{"op":"replace","path":"/body/0/film_counts/2","value":"26","fromValue":23,"resolution":"ERR_ValTypeMismatch"}\n' +
+            '{"op":"replace","path":"/body/0/timestamp","value":27523407007561,"fromValue":1641491700530174,"resolution":"OK_OptionalMismatch"}\n' +
+            '{"op":"remove","path":"/body/0/book_info","value":{"reviews":[{"reviewer":"Reviewer1","text":"An extremely entertaining play by Shakespeare. The slapstick humour is refreshing!"},{"reviewer":"Reviewer2","text":"Absolutely fun and entertaining. The play lacks thematic depth when compared to other plays by Shakespeare."}],"id":"851"},"resolution":"OK"}';
 
         var textedJson = JSON.stringify(recordedResponse, undefined, 4);
         var textedJson1 = JSON.stringify(replayRes1, undefined, 4);
