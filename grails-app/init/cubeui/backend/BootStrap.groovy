834a2d86f222edc1da107e797d338085ec75e916
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-04-29 16:49:19 +0530

    Sample Product domain-class added for test

--- grails-app/init/cubeui/backend/BootStrap.groovy
+++ grails-app/init/cubeui/backend/BootStrap.groovy
@@ -3,6 +3,9 @@ package cubeui.backend
 class BootStrap {
 
     def init = { servletContext ->
+        def prod1 = new Product(prodName: "iPhone 7", prodDesc: "New iPhone 7 32GB", prodPrice: 780).save flush:true
+        def prod2 = new Product(prodName: "iPhone 7 Plus", prodDesc: "New iPhone 7 Plus 128GB", prodPrice: 990).save flush:true
+        def prod3 = new Product(prodName: "iPhone 7 SE", prodDesc: "New iPhone 7 SE 64GB", prodPrice: 520).save flush:true
     }
     def destroy = {
     }
