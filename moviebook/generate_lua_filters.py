d87497307096eed7bd0a8b90ebbd61bb5fedbfb6
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-05-08 17:43:05 +0530

    Add staging environment

--- moviebook/generate_lua_filters.py
+++ moviebook/generate_lua_filters.py
@@ -10,6 +10,7 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('moviebook/templates'))
 customer = sys.argv[1]
+namespace = sys.argv[2]
 cube_application = os.environ['CUBE_APPLICATION']
 cube_instanceid = os.environ['CUBE_INSTANCEID']
 data_stream = open('moviebook/services.yaml', 'r')
@@ -20,8 +21,8 @@ template_replay = env.get_template('moviebook_replay.j2')
 with open("moviebook/moviebook-envoy-cs.yaml", "w") as record:
     with open("moviebook/moviebook-envoy-replay-cs.yaml", "w") as replay:
         for service in dataMap['services']:
-            output_from_record_template = template_record.render(service_name=service,customer=customer, cube_application=cube_application, cube_instanceid=cube_instanceid)
-            output_from_replay_template = template_replay.render(service_name=service,customer=customer, cube_application=cube_application, cube_instanceid=cube_instanceid)
+            output_from_record_template = template_record.render(service_name=service,customer=customer, cube_application=cube_application, cube_instanceid=cube_instanceid, namespace=namespace)
+            output_from_replay_template = template_replay.render(service_name=service,customer=customer, cube_application=cube_application, cube_instanceid=cube_instanceid, namespace=namespace)
             record.write("---\n")
             replay.write("---\n")
             record.write(output_from_record_template + "\n" * 2)
