2b9e0a6c624c7bf79d785847f7dd03e774f672ea
dhiraj_bh <dhiraj@beehyv.com>
2019-05-09 14:54:31 +0530

    Diff in progress

--- client/common/components/Diff.js
+++ client/common/components/Diff.js
@@ -1,35 +1,159 @@
 import React, { Component } from 'react';
+import './Diff.css'
 
 class Diff extends Component {
     constructor(props) {
         super(props);
     }
 
-    formatData() {
-        const {resByPath} =  this.props;
-        let temp = [];
-        let formattedList = [];
-        for (const dp of resByPath) {
-            if (dp.path && dp.service) {
-                if (temp.indexOf(dp.service) == -1) {
-                    temp.push(dp.service);
+    formatDiff() {
+        let {diff} = this.props;
+        let formattedDiff = [];
+        let pathList = [];
+        for (const d of diff) {
+            pathList.push(d.path);
+        }
+
+        for (let i =  0; i < diff.length; i++) {
+            const d = diff[i];
+            let pArr = pathList[i].split('/');
+            pArr.shift();
+            let li = pArr.pop();
+            pArr =  pArr.join('--');
+            let type = 'obj';
+            if (parseInt(li) == li) {
+                type = 'array'
+            }
+
+            if (d.type == 'obj') {
+                formattedDiff.push({
+                    pArr: pArr,
+                    type: type,
+                    children: [d]
+                });
+            } else {
+                let result = formattedDiff.filter(obj => {
+                    return obj.pArr === pArr
+                });
+
+                if (result && result[0] && result[0].children) {
+                    result[0].children.push(d);
+                } else {
+                    formattedDiff.push({
+                        pArr: pArr,
+                        type: type,
+                        children: [d]
+                    });
                 }
             }
         }
 
-        for (const key in temp) {
-
-        }
-
-        console.log(temp);
+        return (formattedDiff);
     }
 
     render() {
         let {recorded, replayRes, diff} = this.props;
+        const formattedDiff = this.formatDiff();
+
+        let formattedDiffElements = formattedDiff.map((d, i, fde) => {
+            const keys = d.pArr.indexOf('--') != -1 ? d.pArr.split('--') : [d.pArr];
+
+            let childrenElem = d.children.map(c => {
+                console.log(c.path);
+                let ck = c.path.indexOf('/') != -1 ? c.path.split('/') : [c.path];
+                let tempBef = JSON.parse(JSON.stringify(recorded));
+                ck.shift();
+                /*keys.shift();
+                for (const key of ck) {
+                    temp = temp[key];
+                }*/
+
+                for (let i = 0; i < ck.length; i++) {
+                    tempBef = tempBef[ck[i]];
+                }
+
+                if (c.op == 'remove') {
+                    return (
+                        <div key={c.path} className="tabbed">
+                            <span>{ck[ck.length-1] + ': '}</span>
+                            <pre><div className="removed">{JSON.stringify(c.value, undefined, 4)}</div></pre>
+                        </div>
+                    )
+                } else if (c.op == 'replace') {
+                    return (
+                        <div key={c.path} className="tabbed">
+                            <span>{ck[ck.length-1] + ': '}</span>
+                            <span className="removed">{c.fromValue}</span>&nbsp;
+                            <span className="added">{c.value}</span>
+                        </div>
+                    )
+                } else if (c.op == 'add') {
+                    return (
+                        <div key={c.path} className="tabbed">
+                            <span>{ck[ck.length-1] + ': '}</span>
+                            <span className="removed">{c.fromValue ? c.fromValue : tempBef}</span>&nbsp;
+                            <span className="added">{c.value}</span>
+                        </div>
+                    )
+                }
+            });
+
+            if (d.type == 'array') {
+                return (
+                    <div key={d.pArr}>
+                        <div>{keys[keys.length-1] + ': ['}</div>
+                        {childrenElem}
+                        <div>]</div>
+                    </div>
+                )
+            } else {
+                return (
+                    <div key={d.pArr}>
+                        <div>{keys[keys.length-1] + ': {'}</div>
+                        {childrenElem}
+                        <div>}</div>
+                    </div>
+                )
+            }
+        });
+
+        let diffElems = diff.map((d, i, darr) => {
+            const keys = d.path.split('/');
+            let temp = JSON.parse(JSON.stringify(recorded));
+            let tempBef = JSON.parse(JSON.stringify(recorded));
+            keys.shift();
+            for (const key of keys) {
+                temp = temp[key];
+            }
+
+            for (let i = 0; i < keys.length - 1; i++) {
+                tempBef = tempBef[keys[i]];
+            }
+
+            if (d.op == 'remove') {
+                return (
+                    <pre>
+                        {keys[keys.length-1] + ': '}
+                        <span className="removed">
+                            {JSON.stringify(temp, undefined, 4)}
+                        </span>
+                    </pre>
+                )
+            } else {
+                return (
+                    <pre>
+                        {keys[keys.length-1] + ': '}
+                        {JSON.stringify(tempBef, undefined, 4)}&nbsp;&nbsp;
+                        {JSON.stringify(d.value, undefined, 4)}
+                    </pre>
+                )
+            }
+
+        });
 
         return (
             <div style={{marginTop: '20px'}}>
-                This is diff
+                {formattedDiffElements}
             </div>
         )
     }
