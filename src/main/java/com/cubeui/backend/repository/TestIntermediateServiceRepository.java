7fb9a5c4eee14f015df0fb4d7a0640aa29c2e5eb
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-05-14 19:21:04 +0530

    Unnecessary files removed

--- src/main/java/com/cubeui/backend/repository/TestIntermediateServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cubeui.backend.repository;
-
-import com.cubeui.backend.domain.TestIntermediateService;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "test_intermediate_services", collectionResourceRel = "test_intermediate_services", itemResourceRel = "test_intermediate_service")
-public interface TestIntermediateServiceRepository extends JpaRepository<TestIntermediateService, Long> {
-}
--- src/main/java/com/cubeui/backend/repository/TestVirtualizedServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cubeui.backend.repository;
-
-import com.cubeui.backend.domain.TestVirtualizedService;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "test_virtualized_services", collectionResourceRel = "test_virtualized_services", itemResourceRel = "test_virtualized_service")
-public interface TestVirtualizedServiceRepository extends JpaRepository<TestVirtualizedService, Long> {
-}
