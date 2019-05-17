9a40b53b83a96872c8f02aca35755ec36f9efec6
Ravi Vijay <ravivj@random.local>
2019-05-17 08:51:05 +0530

    Further changes to schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -31,20 +31,20 @@ CREATE TYPE cube.instance_name AS ENUM ('Prod' , 'Dev' , 'Staging');
 CREATE TABLE cube.instance (
   id SERIAL PRIMARY KEY,
   name cube.instance_name NOT NULL,
+  gateway_endpoint TEXT NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE cube.app (
   id BIGSERIAL PRIMARY KEY,
   customer_id BIGINT REFERENCES cube.cubeuser(id) ON DELETE CASCADE,
-  instance_id INTEGER REFERENCES cube.instance(id) ON DELETE RESTRICT,
   name VARCHAR(200) NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW(),
-  UNIQUE (customer_id, instance_id, name)
+  UNIQUE (customer_id, name)
 );
 
-CREATE INDEX app_index ON cube.app(customer_id, instance_id);
+CREATE INDEX app_index ON cube.app(customer_id);
 
 CREATE TRIGGER set_timestamp_app
 BEFORE UPDATE ON cube.app
@@ -56,14 +56,13 @@ CREATE TYPE cube.service_type AS ENUM ('gateway', 'intermediate', 'virtualized')
 CREATE TABLE cube.service (
   id BIGSERIAL PRIMARY KEY,
   app_id  BIGINT REFERENCES cube.app(id) ON DELETE CASCADE,
-  type cube.service_type NOT NULL,
   name VARCHAR(200) NOT NULL,
   created_at TIMESTAMPTZ DEFAULT NOW(),
   updated_at TIMESTAMPTZ DEFAULT NOW(),
   UNIQUE (app_id, name)
 );
 
-CREATE INDEX service_index ON cube.service(app_id,type);
+CREATE INDEX service_index ON cube.service(app_id);
 
 CREATE TRIGGER set_timestamp_service
 BEFORE UPDATE ON cube.service
@@ -89,15 +88,16 @@ CREATE TYPE cube.recording_status AS ENUM ('Running' , 'Completed' , 'Error');
 CREATE TABLE cube.recording (
   id BIGSERIAL PRIMARY KEY,
   app_id BIGINT REFERENCES cube.app(id) ON DELETE CASCADE,
+  instance_id BIGINT REFERENCES cube.instance(id) ON DELETE CASCADE,
   collection_name VARCHAR(200) NOT NULL,
   status cube.recording_status NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   completed_at TIMESTAMPTZ,
-  UNIQUE(app_id, collection_name)
+  UNIQUE(app_id, instance_id, collection_name)
 );
 
-CREATE INDEX recording_index ON cube.recording(app_id, status);
+CREATE INDEX recording_index ON cube.recording(app_id, instance_id, status);
 
 CREATE TRIGGER set_timestamp_recording
 BEFORE UPDATE ON cube.recording
@@ -105,30 +105,33 @@ FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 /*can this collection be a range instead of a single collection*/
-create TABLE cube.test (
+create TABLE cube.test_config (
   id BIGSERIAL PRIMARY KEY,
   test_config_name TEXT NOT NULL,
+  app_id BIGINT REFERENCES cube.app(id) ON DELETE CASCADE,
   description TEXT,
-  collection_id BIGINT REFERENCES cube.recording(id) ON DELETE CASCADE,
   gateway_service_id BIGINT REFERENCES cube.service(id) ON DELETE CASCADE,
   gateway_path_selection JSON NOT NULL,
-  endpoint TEXT NOT NULL,
+  gateway_req_selection JSON,
+  max_run_time_min INTEGER,
+  emai_id TEXT,
+  slack_id TEXT,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  UNIQUE (collection_id , test_config_name)
+  UNIQUE (app_id , test_config_name)
 );
 
-CREATE INDEX test_index ON cube.test(collection_id);
+CREATE INDEX test_index ON cube.test_config(app_id);
 
 CREATE TRIGGER set_timestamp_test
-BEFORE UPDATE ON cube.test
+BEFORE UPDATE ON cube.test_config
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
 /*need to somehow make sure that the test id and service id correspond to the same app*/
 create TABLE cube.test_virtualized_service (
-  test_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
-  service_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
+  test_id BIGINT REFERENCES cube.test_config(id) ON DELETE CASCADE,
+  service_id BIGINT REFERENCES cube.service(id) ON DELETE CASCADE,
   UNIQUE (test_id , service_id)
 );
 
@@ -136,8 +139,8 @@ CREATE INDEX virtualized_service_index ON cube.test_virtualized_service(test_id)
 
 /*need to somehow make sure that the test id and service id correspond to the same app*/
 create TABLE cube.test_intermediate_service (
-  test_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
-  service_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
+  test_id BIGINT REFERENCES cube.test_config(id) ON DELETE CASCADE,
+  service_id BIGINT REFERENCES cube.service(id) ON DELETE CASCADE,
   UNIQUE(test_id, service_id)
 );
 
@@ -145,21 +148,22 @@ CREATE INDEX intermediate_service_index ON cube.test_intermediate_service(test_i
 
 CREATE TYPE cube.template_type AS ENUM ('Request' , 'Response');
 
-create TABLE cube.compare_template (
+create TABLE cube.comparision_template (
   id BIGSERIAL PRIMARY KEY,
-  test_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
+  app_id BIGINT REFERENCES cube.app(id) ON DELETE CASCADE,
+  service_id BIGINT REFERENCES cube.service(id) ON DELETE CASCADE,
   path TEXT NOT NULL,
   template JSON NOT NULL,
   type cube.template_type NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  UNIQUE(test_id, type, path)
+  UNIQUE(app_id, service_id, type, path)
 );
 
-CREATE INDEX compare_template_index ON cube.compare_template(test_id, type, path);
+CREATE INDEX compare_template_index ON cube.comparision_template(app_id, service_id, type, path);
 
 CREATE TRIGGER set_timestamp_template
-BEFORE UPDATE ON cube.compare_template
+BEFORE UPDATE ON cube.comparision_template
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
@@ -171,7 +175,8 @@ Not creating a separate analysis table, including analysis as a json here itself
 CREATE TABLE cube.replay (
   id BIGSERIAL PRIMARY KEY,
   replay_name VARCHAR(200) NOT NULL,
-  test_id BIGINT REFERENCES cube.test(id) ON DELETE CASCADE,
+  test_config_id BIGINT REFERENCES cube.test_config(id) ON DELETE CASCADE,
+  collection_id BIGINT REFERENCES cube.recording(id) ON DELETE CASCADE,
   status cube.replay_status NOT NULL,
   req_count INTEGER,
   req_sent INTEGER,
@@ -181,10 +186,10 @@ CREATE TABLE cube.replay (
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   completed_at TIMESTAMPTZ,
   sample_rate REAL,
-  UNIQUE(test_id, replay_name)
+  UNIQUE(test_config_id, collection_id, replay_name)
 );
 
-CREATE INDEX replay_index ON cube.replay(test_id, status);
+CREATE INDEX replay_index ON cube.replay(test_config_id, collection_id, status);
 
 CREATE TRIGGER set_timestamp_replay
 BEFORE UPDATE ON cube.replay
