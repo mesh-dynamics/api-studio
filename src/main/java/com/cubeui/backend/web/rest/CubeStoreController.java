6859c7cf062a6f0ebfa54188fc8e0d9f18c72917
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-05-21 17:20:07 +0530

    POST methods implemented in CubeStoreController

--- src/main/java/com/cubeui/backend/web/rest/CubeStoreController.java
+++ src/main/java/com/cubeui/backend/web/rest/CubeStoreController.java
@@ -8,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Optional;
+
 import static com.cubeui.backend.security.Constants.CUBE_SERVER_HREF;
 import static org.springframework.http.ResponseEntity.*;
 
@@ -46,60 +48,38 @@ public class CubeStoreController {
     }
 
     @PostMapping(value = "/req")
-    public ResponseEntity storereq(@RequestBody String req) { // Edited: Request req
-        return noContent().build();
+    public ResponseEntity storereq(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping("/resp")
-    public ResponseEntity storeresp(@RequestBody String resp) { // Edited: Response resp
-        return noContent().build();
+    public ResponseEntity storeresp(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping(value = "/fr", consumes = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity storeFunc(String functionReqRespString /* @PathParam("customer") String customer,
-                              @PathParam("instance") String instance, @PathParam("app") String app,
-                              @PathParam("service") String service*/) {
-        return noContent().build();
+    public ResponseEntity storeFunc(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping(value = "/setdefault/{customerid}/{app}/{serviceid}/{method}/{var:.+}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity setDefault(
-//            @Context UriInfo ui,
-            @PathVariable("var") String path,
-//            MultivaluedMap<String, String> formParams,
-            @PathVariable("customerid") String customerid,
-            @PathVariable("app") String app,
-            @PathVariable("serviceid") String serviceid,
-            @PathVariable("method") String method) {
-        return noContent().build();
+    public ResponseEntity setDefault(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping("/setdefault/{method}/{var:.+}")
-    public ResponseEntity setDefaultFullResp(
-//            @Context UriInfo ui,
-            @PathVariable("var") String path,
-            @RequestBody String resp, // Edited: com.cube.dao.Response resp,
-            @PathVariable("method") String method) {
-        return noContent().build();
+    public ResponseEntity setDefaultFullResp(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping(value = "/start/{customerid}/{app}/{instanceid}/{collection}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity start(
-//            @Context UriInfo ui,
-                          @PathVariable("app") String app,
-                          @PathVariable("customerid") String customerid,
-                          @PathVariable("instanceid") String instanceid,
-                          @PathVariable("collection") String collection) {
-        return noContent().build();
+    public ResponseEntity start(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
     @PostMapping("/stop/{customerid}/{app}/{collection}")
-    public ResponseEntity stop(
-//            @Context UriInfo ui,
-            @PathVariable("collection") String collection,
-            @PathVariable("customerid") String customerid,
-            @PathVariable("app") String app) {
-        return noContent().build();
+    public ResponseEntity stop(HttpServletRequest request, @RequestBody String requestBody) {
+        return cubeServerService.fetchPostResponse(request, Optional.ofNullable(requestBody));
     }
 
 }
