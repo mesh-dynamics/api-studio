324cec0b1da4d6b88a9a712ad9fc4812801ce559
Prasad M Deshpande <prasad.deshpande@cubecorp.io>
2019-05-29 11:03:24 +0530

    small fix

--- src/main/java/com/cube/dao/SolrIterator.java
+++ src/main/java/com/cube/dao/SolrIterator.java
@@ -21,7 +21,6 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.params.SolrParams;
-import org.checkerframework.checker.nullness.Opt;
 
 import io.cube.agent.FnKey;
 
