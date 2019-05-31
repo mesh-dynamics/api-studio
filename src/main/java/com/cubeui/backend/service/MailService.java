e3d20293356d2f04df21c8ff0c7ffa8d67464d45
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-05-31 20:48:17 +0530

    MailService Created

---
+++ src/main/java/com/cubeui/backend/service/MailService.java
@@ -0,0 +1,102 @@
+package com.cubeui.backend.service;
+
+import com.cubeui.backend.domain.User;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.core.SpringProperties;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import org.thymeleaf.context.Context;
+import org.apache.commons.lang3.CharEncoding;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.mail.internet.MimeMessage;
+import java.util.Locale;
+
+import static com.cubeui.backend.security.Constants.DEFAULT_LANGUAGE;
+
+@Slf4j
+@Service
+@Transactional
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+    private final MessageSource messageSource;
+    private final SpringTemplateEngine templateEngine;
+
+    @Value("${server.baseUrl}")
+    private String baseUrl;
+
+    public MailService(JavaMailSender javaMailSender, MessageSource messageSource, SpringTemplateEngine templateEngine) {
+        this.javaMailSender = javaMailSender;
+        this.messageSource = messageSource;
+        this.templateEngine = templateEngine;
+    }
+
+    public void sendEmail() {
+//        SimpleMailMessage msg = new SimpleMailMessage();
+//        msg.setTo("vineetks.iitk@gmail.com", "to_2@gmail.com", "to_3@yahoo.com");
+//        msg.setSubject("Testing from Spring Boot");
+//        msg.setText("Hello World \n Spring Boot Email");
+//        javaMailSender.send(msg);
+    }
+
+    @Async
+    public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        log.debug("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+                isMultipart, isHtml, to, subject, content);
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, CharEncoding.UTF_8);
+            message.setTo(to);
+//            message.setFrom(jHipsterProperties.getMail().getFrom());
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            javaMailSender.send(mimeMessage);
+            log.debug("Sent email to User '{}'", to);
+        } catch (Exception e) {
+            if (log.isDebugEnabled()) {
+                log.warn("Email could not be sent to user '{}'", to, e);
+            } else {
+                log.warn("Email could not be sent to user '{}': {}", to, e.getMessage());
+            }
+        }
+    }
+
+    @Async
+    public void sendEmailFromTemplate(User user, String templateName, String titleKey) {
+        Locale locale = Locale.forLanguageTag(DEFAULT_LANGUAGE);
+        Context context = new Context();
+        context.setVariable("user", user);
+        context.setVariable("baseUrl", baseUrl);
+        String content = templateEngine.process(templateName, context);
+        String subject = messageSource.getMessage(titleKey, null, locale);
+        sendEmail(user.getUsername(), subject, content, true, true);
+    }
+
+    @Async
+    public void sendActivationEmail(User user) {
+        log.debug("Sending activation email to '{}'", user.getUsername());
+        sendEmailFromTemplate(user, "activationEmail", "email.activation.title");
+    }
+
+    @Async
+    public void sendCreationEmail(User user) {
+        log.debug("Sending creation email to '{}'", user.getUsername());
+        sendEmailFromTemplate(user, "creationEmail", "email.creation.title");
+    }
+
+    @Async
+    public void sendPasswordResetMail(User user) {
+        log.debug("Sending password reset email to '{}'", user.getUsername());
+        sendEmailFromTemplate(user, "passwordResetEmail", "email.reset.title");
+    }
+
+}
