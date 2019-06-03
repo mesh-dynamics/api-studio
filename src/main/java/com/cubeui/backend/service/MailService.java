bb046291aa285cc627fd540cf722002685c31474
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-06-03 18:24:51 +0530

    Mail SMTP bug Fixed, Mail Service Improved

--- src/main/java/com/cubeui/backend/service/MailService.java
+++ src/main/java/com/cubeui/backend/service/MailService.java
@@ -30,8 +30,17 @@ public class MailService {
     private final MessageSource messageSource;
     private final SpringTemplateEngine templateEngine;
 
-    @Value("${server.baseUrl}")
+    @Value("${spring.mail.frontend.baseUrl}")
     private String baseUrl;
+
+    @Value("${spring.mail.frontend.activationEndpoint}")
+    private String activationEndpoint;
+
+    @Value("${spring.mail.frontend.loginEndpoint}")
+    private String loginEndpoint;
+
+    @Value("${spring.mail.frontend.resetEndpoint}")
+    private String resetEndpoint;
 
     @Value("${spring.mail.username}")
     private String emailSender;
@@ -42,18 +51,19 @@ public class MailService {
         this.templateEngine = templateEngine;
     }
 
+    @Async
     public void sendEmail() {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo("abc@gmail.com", "xyz@gmail.com", "def@yahoo.com");
-        msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n Spring Boot Email");
+        msg.setSubject("Test email from CubeIO");
+        msg.setText("Hello World! \n Regards:\n CubeIO");
         msg.setFrom(emailSender);
         javaMailSender.send(msg);
     }
 
     @Async
     public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
-        log.debug("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+        log.debug("Send email [multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
                 isMultipart, isHtml, to, subject, content);
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
@@ -62,8 +72,10 @@ public class MailService {
             message.setFrom(emailSender);
             message.setSubject(subject);
             message.setText(content, isHtml);
+            long t = System.currentTimeMillis();
             javaMailSender.send(mimeMessage);
-            log.debug("Sent email to User '{}'", to);
+            System.out.println(System.currentTimeMillis()-t);
+            log.debug("Email sent to User '{}'", to);
         } catch (Exception e) {
             if (log.isDebugEnabled()) {
                 log.warn("Email could not be sent to user '{}'", to, e);
@@ -78,11 +90,12 @@ public class MailService {
         Locale locale = Locale.forLanguageTag(DEFAULT_LANGUAGE);
         Context context = new Context();
         context.setVariable("user", user);
-        context.setVariable("baseUrl", baseUrl);
         //TODO Get frontend login url and change it
-        context.setVariable("loginUrl", baseUrl);
+        context.setVariable("loginUrl", baseUrl + loginEndpoint);
         //TODO Get frontend reset url and change it
-        context.setVariable("resetUrl", baseUrl);
+        context.setVariable("resetUrl", baseUrl + resetEndpoint);
+        //TODO Get frontend reset url and change it
+        context.setVariable("activationUrl", baseUrl + activationEndpoint);
         String content = templateEngine.process(templateName, context);
         String subject = messageSource.getMessage(titleKey, null, locale);
         sendEmail(user.getUsername(), subject, content, true, true);
