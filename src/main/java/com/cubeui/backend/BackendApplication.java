6ff990e50c1c1a0f30d5fbe3af0ed2c2b31cd83e
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-06-03 18:25:18 +0530

    Async Implemented to send emails

--- src/main/java/com/cubeui/backend/BackendApplication.java
+++ src/main/java/com/cubeui/backend/BackendApplication.java
@@ -3,10 +3,11 @@ package com.cubeui.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@EnableAsync
 @SpringBootApplication
 public class BackendApplication {
 
