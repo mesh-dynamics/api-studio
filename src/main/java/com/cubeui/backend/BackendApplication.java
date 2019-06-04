af8718761d39bb4dae2c5599e187c9d67942f08b
Vineet Kumar Singh <vineetkr.999@gmail.com>
2019-06-04 20:25:38 +0530

    Default Profile added

--- src/main/java/com/cubeui/backend/BackendApplication.java
+++ src/main/java/com/cubeui/backend/BackendApplication.java
@@ -1,18 +1,34 @@
 package com.cubeui.backend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.cubeui.backend.security.Constants.SPRING_PROFILE_DEFAULT;
+import static com.cubeui.backend.security.Constants.SPRING_PROFILE_DEVELOPMENT;
+
+@Slf4j
 @EnableAsync
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+//        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication app = new SpringApplication(BackendApplication.class);
+        addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
     }
 
     @Bean
@@ -20,4 +36,43 @@ public class BackendApplication {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n" +
+                        "-------------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n" +
+                        "-------------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+
+    private static void addDefaultProfile(SpringApplication app) {
+        Map<String, Object> defProperties = new HashMap<>();
+        defProperties.put(SPRING_PROFILE_DEFAULT, SPRING_PROFILE_DEVELOPMENT);
+        app.setDefaultProperties(defProperties);
+    }
 }
--- src/main/java/com/cubeui/backend/security/Constants.java
+++ src/main/java/com/cubeui/backend/security/Constants.java
@@ -6,6 +6,8 @@ public final class Constants {
     public static final String DEFAULT_LANGUAGE = "en";
     public static final int PASSWORD_MIN_LENGTH = 4;
     public static final int PASSWORD_MAX_LENGTH = 100;
+    public static final String SPRING_PROFILE_DEFAULT = "spring.profiles.default";
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
 
     private Constants() {
     }
