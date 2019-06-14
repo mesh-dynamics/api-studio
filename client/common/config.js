a9e6be232780ccf23260c9484963b3e2af6264aa
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-06-14 17:04:24 +0530

    Change base URL to /cube (#20)
    relative path send back the request to the same server, i.e.
    demo.cubecorp.io or staging.cubecorp.io.
    /cube is then handled by istio

--- client/common/config.js
+++ client/common/config.js
@@ -1,8 +1,6 @@
 const config = {
     apiUrl: "http://localhost:9997",
-    baseUrl: "http://demo.cubecorp.io"
-    //baseUrl: "http://staging.cubecorp.io"
-    //baseUrl: "http://a46d07a274e5311e98fc60264eb36f9a-626201257.us-east-2.elb.amazonaws.com"
+    baseUrl: "/cube"
 };
 
 export default config;
