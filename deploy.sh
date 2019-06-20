673d71f6ccc12f750c8f39825bd63163af43d31f
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-06-20 20:48:13 +0530

    Add deloy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#syntax to use this script ./deploy.sh appname operation Configration
+
+generate_menifest() {
+	APP_CONF=$APP_DIR/config/"$1".conf
+	if [ ! -f "$APP_CONF" ]; then #Check if config file exist
+		echo "Configration files does not exist"
+		exit 1
+	fi
+	if [ "$OPERATION" = "init" ]; then
+		source $APP_CONF
+		COMMON_DIR=common
+		./generate_yamls.py $COMMON_DIR $NAMESPACE $NAMESPACE_HOST
+		./generate_yamls.py $APP_DIR $NAMESPACE $NAMESPACE_HOST
+	fi
+}
+
+main () {
+	# To debug this script, run it with TRACE=1 in the enviornment
+	# -x option will trace each command that is run
+	set -o pipefail; [[ "$TRACE" ]] && set -x
+	if [ -d $1 ]; then #Check if the App directory exists
+		APP_DIR=$1
+		shift
+	else
+		echo "App directory does not exist"
+		exit 1 #Exist with nonzero exit code
+	fi
+	case "$1" in
+		init) OPERATION="init"; shift; generate_menifest $1;;
+	esac
+}
+
+main "$@"
