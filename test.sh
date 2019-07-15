2fe1bacd8d1953943b54cbc45faae8e413802390
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-07-15 17:35:43 +0530

    Add debug

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -x
 #Init Replay
 REPLAY_ID=$(curl -X POST \
 	http://dogfooding.cubecorp.io/cube/rs/init/demo@cubecorp.io/Cube/dogfood-14-june-1 \
@@ -7,18 +8,21 @@ REPLAY_ID=$(curl -X POST \
 	-H 'cache-control: no-cache' \
 	-d 'endpoint=http://staging.cubecorp.io&instanceid=PROD' | awk -F ',' '{print $7}' | cut -d '"' -f 4)
 
+echo "Init done"
 #Start replay
 curl -f -X POST \
   http://dogfooding.cubecorp.io/rs/start/demo@cubecorp.io/Cube/dogfood-14-june-1/$REPLAY_ID \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H 'cache-control: no-cache'
 
+echo "replay started"
 #Status Check
 while [ "$STATUS" != "Completed" ]; do
 	STATUS=$(curl -X GET http://dogfooding.cubecorp.io/rs/status/demo@cubecorp.io/Cube/dogfood-14-june-1/$REPLAY_ID | awk -F ',' '{print $9}' | cut -d '"' -f 4)
 	sleep 5
 done
 
+echo "done with while"
 #Run analyze
 ANALYZE=$(curl -X POST http://dogfooding.cubecorp.io/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
 REQNOTMATCHED=$(echo $ANALYZE | awk -F ',' '{print $8}' | cut -d '"' -f 4)
