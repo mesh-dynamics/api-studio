35c0b9800537d7c997d685a23f5775dce1b1de51
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-07-15 17:25:04 +0530

    Add status check and analyze

--- test.sh
+++ test.sh
@@ -13,5 +13,24 @@ curl -f -X POST \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H 'cache-control: no-cache'
 
-#print replay ID
+#Status Check
+while [ "$STATUS" != "Completed" ]; do
+	STATUS=$(curl -X GET http://dogfooding.cubecorp.io/rs/status/demo@cubecorp.io/Cube/dogfood-14-june-1/$REPLAY_ID | awk -F ',' '{print $9}' | cut -d '"' -f 4)
+	sleep 5
+done
+
+#Run analyze
+ANALYZE=$(curl -X POST http://dogfooding.cubecorp.io/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
+REQNOTMATCHED=$(echo $ANALYZE | awk -F ',' '{print $8}' | cut -d '"' -f 4)
+RESPNOTMATCHED=$(echo $ANALYZE | awk -F ',' '{print $11}' | cut -d '"' -f 4)
+
+#Display replay ID
 echo "Replay ID:" $REPLAY_ID
+#Exit with non-zero exit code if reqstnotmatched and respnotmatchted are have nono-zero value
+if [ "$REQNOTMATCHED" = "0" ] && [ "$RESPNOTMATCHED" = "0" ];then
+	echo "test passed"
+	exit 0
+else
+	echo "test failed"
+	exit 1
+fi
