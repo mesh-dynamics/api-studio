3f019910fdc68f6559e7e77ced0f2f7dad60d708
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-07-18 15:39:20 +0530

    repalce app specific code with generic code

--- generate_yamls.py
+++ generate_yamls.py
@@ -53,13 +53,16 @@ def recap_files(operation, base_dir, template_dir, namespace, cube_application, 
             out.write("---\n")
             out.write(output_from_template + "\n" * 2)
     out.close()
-    if base_dir == "apps/moviebook" and operation == "replay":
-        outfile = base_dir + "/kubernetes/mock-all-except-moviebook.yaml"
-        template = env.get_template("mock-all-except-moviebook.j2")
-        with open(outfile, "w") as out:
-            output_from_template = template.render(namespace=namespace, customer=cube_customer, cube_application=cube_application, cube_instance=cube_instanceid)
-            out.write(output_from_template)
-            out.close()
+    if operation == "replay":
+        app = os.path.split(base_dir)
+        mock_file = base_dir + "/templates/mock-all-except-" + app[1] + ".j2"
+        if os.path.isfile(mock_file):
+            outfile = base_dir + "/kubernetes/mock-all-except-" + app[1] + ".yaml"
+            template = env.get_template("mock-all-except-" + app[1] + ".j2")
+            with open(outfile, "w") as out:
+                output_from_template = template.render(namespace=namespace, customer=cube_customer, cube_application=cube_application, cube_instance=cube_instanceid)
+                out.write(output_from_template)
+                out.close()
 
 if __name__ == "__main__":
     main()
