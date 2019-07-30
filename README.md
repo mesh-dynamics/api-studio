51c087a2e2e9e71691ce204112d94193dee5ebfc
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-07-30 15:28:38 +0530

    Fix/redis auth (#149)
    * test commit
    
    * Fix redis auth
    
    * Update URl

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# cubeio
+# CubeIO
 
 [![Build Status](http://drone.cubecorp.io/api/badges/cube-io-corp/cubeio/status.svg)](http://drone.cubecorp.io/cube-io-corp/cubeio)
--- test.sh
+++ test.sh
@@ -3,25 +3,25 @@
 set -x
 #Init Replay
 REPLAY_ID=$(curl -X POST \
-	http://dogfooding.cubecorp.io/cube/rs/init/demo@cubecorp.io/Cube/dogfood-14-june-1 \
+	http://demo.dev.cubecorp.io/cube/rs/init/CubeCorp/Cube/dogfood-14-june-1 \
 	-H 'Content-Type: application/x-www-form-urlencoded' \
 	-H 'cache-control: no-cache' \
-	-d 'endpoint=http://staging.cubecorp.io&instanceid=PROD' | awk -F ',' '{print $7}' | cut -d '"' -f 4)
+	-d 'endpoint=http://staging.dev.cubecorp.io&instanceid=PROD' | awk -F ',' '{print $7}' | cut -d '"' -f 4)
 
 #Start replay
 curl -f -X POST \
-  http://dogfooding.cubecorp.io/rs/start/demo@cubecorp.io/Cube/dogfood-14-june-1/$REPLAY_ID \
+  http://demo.dev.cubecorp.io/rs/start/CubeCorp/Cube/dogfood-14-june-1/$REPLAY_ID \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H 'cache-control: no-cache'
 
 #Status Check
 while [ "$STATUS" != "Completed" ] && [ "$STATUS" != "Error" ]; do
-	STATUS=$(curl -X GET http://dogfooding.cubecorp.io/rs/status/demo@cubecorp.io/Cube/dogfood-14-june-1/$REPLAY_ID | awk -F ',' '{print $9}' | cut -d '"' -f 4)
+	STATUS=$(curl -X GET http://demo.dev.cubecorp.io/rs/status/CubeCorp/Cube/dogfood-14-june-1/$REPLAY_ID | awk -F ',' '{print $9}' | cut -d '"' -f 4)
 	sleep 5
 done
 
 #Run analyze
-ANALYZE=$(curl -X POST http://dogfooding.cubecorp.io/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
+ANALYZE=$(curl -X POST http://demo.dev.cubecorp.io/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
 REQNOTMATCHED=$(echo $ANALYZE | awk -F ',' '{print $8}' | cut -d '"' -f 4)
 RESPNOTMATCHED=$(echo $ANALYZE | awk -F ',' '{print $11}' | cut -d '"' -f 4)
 
