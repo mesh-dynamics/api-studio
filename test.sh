c36eba65caa8aee8a667420480eabd587c53a9e9
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2019-09-27 17:09:11 +0530

    modify test golden

--- test.sh
+++ test.sh
@@ -3,21 +3,22 @@
 set -x
 #Init Replay
 REPLAY_ID=$(curl -X POST \
-	http://demo.dev.cubecorp.io/rs/init/CubeCorp/Cube/df-14-aug-12-0f38fd97-4bb3-472d-8bc8-3afd7bccf08d \
+	http://demo.dev.cubecorp.io/rs/init/CubeCorp/Cube/fluentd-test-df-49 \
 	-H 'Content-Type: application/x-www-form-urlencoded' \
 	-H 'cache-control: no-cache' \
 	-d 'endpoint=http://staging.dev.cubecorp.io&instanceid=test&templateSetVer=c86d21a5-82fb-4a4d-bcc8-411177dc2a23'  | sed 's/^.*"replayid":"\([^"]*\)".*/\1/')
 
 #Start replay
 curl -f -X POST \
-  http://demo.dev.cubecorp.io/rs/start/CubeCorp/Cube/df-10-aug-7/$REPLAY_ID \
+  http://demo.dev.cubecorp.io/rs/start/CubeCorp/Cube/fluentd-test-df-49/$REPLAY_ID \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -H 'cache-control: no-cache'
 
 #Status Check
 COUNT=0
 while [ "$STATUS" != "Completed" ] && [ "$STATUS" != "Error" ] && [ "$COUNT" != "20" ]; do
-	STATUS=$(curl -X GET http://demo.dev.cubecorp.io/rs/status/CubeCorp/Cube/df-10-aug-7/$REPLAY_ID | sed 's/^.*"status":"\([^"]*\)".*/\1/')
+	STATUS=$(curl -X GET http://demo.dev.cubecorp.io/rs/status/CubeCorp/Cube/fluentd-test-df-49/$REPLAY_ID | sed 's/^
+	.*"status":"\([^"]*\)".*/\1/')
 	sleep 5
 	COUNT=$((COUNT+1))
 done
