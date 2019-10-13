fe3fd65bea158a0d67452fbb96c27465b09bb420
Paawan <17042021+paawan01@users.noreply.github.com>
2019-10-13 23:31:15 +0530

    Modify ReplayDriver.init to return ReplayDriver itself

--- src/main/java/com/cube/ws/ReplayWS.java
+++ src/main/java/com/cube/ws/ReplayWS.java
@@ -236,7 +236,7 @@ public class ReplayWS {
         if (!running_replays.isEmpty()) {
             return Response.status(Status.FORBIDDEN).entity((new JSONObject(Map.of("Force Complete", new JSONArray(running_replays)))).toString()).build();
         }
-        
+
         // check if recording or replay is ongoing for (customer, app, instanceid)
         Optional<Response> errResp = WSUtils.checkActiveCollection(rrstore, Optional.ofNullable(recording.customerid), Optional.ofNullable(recording.app),
             Optional.ofNullable(recording.instanceid));
@@ -256,18 +256,16 @@ public class ReplayWS {
                         // TODO: introduce response transforms as necessary
                         return ReplayDriver.initReplay(e, recording.customerid, recording.app, recording.instanceid, recording.collection,
                             reqids, rrstore, async, paths, null, samplerate, intermediateServices,templateSetVersion)
-                            .map(replay -> {
+                            .map(replayDriver -> {
                                 String json;
+                                Replay replay = replayDriver.getReplay();
                                 try {
                                     json = jsonmapper.writeValueAsString(replay);
-                                    Optional<ReplayDriver> replayDriver = ReplayDriver.getReplayDriver(replay.replayid, this.rrstore, this.replayResultCache);
-                                    return replayDriver.map(r -> {
-                                        boolean status = r.start();
-                                        if (status) {
-                                            return Response.ok(json, MediaType.APPLICATION_JSON).build();
-                                        }
-                                        return Response.status(Response.Status.CONFLICT).entity("Not able to start replay. It may be already running or completed").build();
-                                    }).orElse(Response.status(Response.Status.NOT_FOUND).entity("Replay not found for replayid: " + replay.replayid).build());
+                                    boolean status = replayDriver.start();
+                                    if (status) {
+                                        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+                                    }
+                                    return Response.status(Response.Status.CONFLICT).entity("Not able to start replay. It may be already running or completed").build();
                                 } catch (JsonProcessingException ex) {
                                     LOGGER.error(String.format("Error in converting Replay object to Json for replayid %s", replay.replayid), ex);
                                     return Response.serverError().build();
