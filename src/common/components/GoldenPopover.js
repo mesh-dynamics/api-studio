c314f0b556546e0dfc240bb385a85fd66cf9e8bd
Dhiraj Kumar <dhiraj@witcurve.com>
2019-10-31 15:04:25 +0530

    removed console.log

--- src/common/components/GoldenPopover.js
+++ src/common/components/GoldenPopover.js
@@ -57,7 +57,6 @@ class GoldenPopover extends React.Component {
     }
 
     updateGolden() {
-        console.log(this.props.serverSideDiff);
         const {dispatch, serverSideDiff, cube} = this.props;
         if (serverSideDiff) {
             let operation = {
