44c8b6e6f2afce3dc977971fb4f6ef409d88e18a
Ravi Vijay <ravi.vijay@cubecorp.io>
2019-11-06 13:23:07 +0530

    Templatize fluentd conf

--- data_coral/data_coral.py
+++ data_coral/data_coral.py
@@ -13,3 +13,10 @@ with open('nginx_mock.conf.j2', 'r') as file:
     rendered = t.render(customer_id='CubeCorp',instance_id='test-dc',app_name='MovieInfo',run_type='Replay',mock_host='staging-sm.dev.cubecorp.io')
     with open('nginx_mock.conf' , 'w') as conf_file:
         conf_file.write(rendered)
+
+with open('fluentd.conf.j2', 'r') as file:
+    data = file.read()
+    t = Template(data)
+    rendered = t.render(path="/var/log/nginx/cube.http.access.log",pos_file="/home/ec2-user/cube.http.access.log.pos",endpoint="http://staging-sm.dev.cubecorp.io/cs/rrbatch",chunk_limit_records="1")
+    with open('fluentd.conf' , 'w') as conf_file:
+        conf_file.write(rendered)
