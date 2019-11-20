f7ad393d36dc3f1e37d2246106f27adfbc7f155b
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2019-11-20 18:48:53 +0530

    revert local changes

--- src/common/config.js
+++ src/common/config.js
@@ -1,8 +1,8 @@
 const config = {
     apiBaseUrl: "http://localhost:8080/api",
-    recordBaseUrl: "http://demo.dev.cubecorp.io/cs",
-    replayBaseUrl: "http://demo.dev.cubecorp.io/rs",
-    analyzeBaseUrl: "http://demo.dev.cubecorp.io/as"
+    recordBaseUrl: "/cs",
+    replayBaseUrl: "/rs",
+    analyzeBaseUrl: "/as"
 };
 
 export default config;
