5e5e35752625e2002266b892ea741a90f6a21971
Ravi Vijay <ravi.vijay@cubecorp.io>
2019-11-21 16:08:13 +0530

    Style changes

--- src/main/java/io/cube/agent/BinaryPayloadDeserializer.java
+++ src/main/java/io/cube/agent/BinaryPayloadDeserializer.java
@@ -1,31 +1,33 @@
 package io.cube.agent;
 
+import java.io.IOException;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import java.io.IOException;
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
 
 public class BinaryPayloadDeserializer extends StdDeserializer<byte[]> {
 
-  protected BinaryPayloadDeserializer() {
-    this(null);
-  }
+	protected BinaryPayloadDeserializer() {
+		this(null);
+	}
 
-  private BinaryPayloadDeserializer(Class<?> vc) {
-    super(vc);
-  }
+	private BinaryPayloadDeserializer(Class<?> vc) {
+		super(vc);
+	}
 
-  @Override
-  public byte[] deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-      throws IOException, JsonProcessingException {
-    String nodeValue = jsonParser.readValueAs(String.class);
-    try {
-      return Hex.decodeHex(nodeValue.toCharArray());
-    } catch (DecoderException e) {
-      return new byte[0];
-    }
-  }
+	@Override
+	public byte[] deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+		throws IOException, JsonProcessingException {
+		String nodeValue = jsonParser.readValueAs(String.class);
+		try {
+			return Hex.decodeHex(nodeValue.toCharArray());
+		} catch (DecoderException e) {
+			return new byte[0];
+		}
+	}
 }
--- src/main/java/io/cube/agent/BinaryPayloadSerializer.java
+++ src/main/java/io/cube/agent/BinaryPayloadSerializer.java
@@ -1,24 +1,26 @@
 package io.cube.agent;
 
+import java.io.IOException;
+
+import org.apache.commons.codec.binary.Hex;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import java.io.IOException;
-import org.apache.commons.codec.binary.Hex;
 
 public class BinaryPayloadSerializer extends StdSerializer<byte[]> {
 
-  protected  BinaryPayloadSerializer() {
-    this(null);
-  }
+	protected BinaryPayloadSerializer() {
+		this(null);
+	}
 
-  private BinaryPayloadSerializer(Class<byte[]> t) {
-    super(t);
-  }
+	private BinaryPayloadSerializer(Class<byte[]> t) {
+		super(t);
+	}
 
-  @Override
-  public void serialize(byte[] bytes, JsonGenerator jsonGenerator,
-      SerializerProvider serializerProvider) throws IOException {
-      jsonGenerator.writeString(Hex.encodeHexString(bytes));
-  }
+	@Override
+	public void serialize(byte[] bytes, JsonGenerator jsonGenerator,
+		SerializerProvider serializerProvider) throws IOException {
+		jsonGenerator.writeString(Hex.encodeHexString(bytes));
+	}
 }
