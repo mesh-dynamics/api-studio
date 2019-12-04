1929c008fe64270bc05d0c09b44f29135c5f8b3d
paawan01 <17042021+paawan01@users.noreply.github.com>
2019-12-04 12:28:39 +0530

    Develop to staging weekly merge (#87)
    * command to set default responses
    
    * Minor fix
    
    * initial commit
    
    * Changes in templates + set error percentage 0 for moviebook (#72)
    
    * Default Response fix
    
    * Minor fix
    
    * camelcase fix
    
    * Minor fix in set_default api
    
    * Fetch image tags from github
    
    * Add labels
    
    * delete based on app label
    
    * Add name and userId field for starting recording (#74)
    
    * Modified deploy script to have set_default command take json file as input
    
    * Add private ingress gateway
    
    * Review comments incorporated
    
    * fix service entry for solr
    
    * Add example to support multi-tanent
    
    * rename istio->IstioSAAAS
    
    * virtual srvice for SAAS solution

--- deploy.sh
+++ deploy.sh
@@ -52,35 +52,31 @@ if [ -z "$1" ]; then
 }
 
 set_default() {
-echo "Setting default responses for RESTWrap!"
+echo "Setting default responses!"
 
-NOW=$(date +%Y-%m-%dT%H:%M:%SZ)
+	if [ -z "$1" ]; then
+		echo "Enter default response file"
+		read FILE_NAME
+	else
+		FILE_NAME=$1
+	fi
 
-# reqId cannot be NA as we are creating Solr doc id with reqId value. Otherwise second request
-# will override the first request here.
+    FILE_PATH=$APP_DIR/default_responses/$FILE_NAME
+    echo "Checking the file path : $FILE_PATH"
+
+    if [ -f $FILE_PATH ]; then
+        FILE_DATA=`jq '.' $FILE_PATH`
+    else
+        echo "File not found : $FILE_PATH"
+        exit 1
+    fi
 
 RESPONSE="$(curl -X POST \
   http://$GATEWAY_URL/cs/event/setDefaultResponse \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
   -H "Host:$CUBE_HOST" \
-  -d '{"event":{"customerId":"CubeCorp","app":"MovieInfo","service":"restwrapjdbc","instanceId":"NA","collection":"NA",
-  "traceId":"NA","runType":"Manual","timestamp":"'$NOW'","reqId":"'$NOW'","apiPath":"restsql/initialize","eventType":"HTTPRequest",
-  "rawPayloadString":"{\"hdrs\":{},\"queryParams\":{},\"formParams\":{},\"method\":\"GET\",\"body\":\"\"}"},
-  "rawRespPayloadString":"{\"hdrs\":{\"content-type\":[\"application\/json\"]},\"body\":\"{status:Connection pool created.}\",\"status\":200}"}')"
-
-echo $RESPONSE
-
-RESPONSE="$(curl -X POST \
-  http://$GATEWAY_URL/cs/event/setDefaultResponse \
-  -H 'Content-Type: application/json' \
-  -H 'cache-control: no-cache' \
-  -H "Host:$CUBE_HOST" \
-  -d '{"event":{"customerId":"CubeCorp","app":"MovieInfo","service":"restwrapjdbc",
-  "instanceId":"NA","collection":"NA","traceId":"NA","runType":"Manual","timestamp":"'$NOW'",
-  "reqId":"'$NOW'","apiPath":"restsql/update","eventType":"HTTPRequest",
-  "rawPayloadString":"{\"hdrs\":{},\"queryParams\":{},\"formParams\":{},\"method\":\"POST\",\"body\":\"\"}"},
-  "rawRespPayloadString":"{\"hdrs\":{\"content-type\":[\"application\/json\"]},\"body\":\"{num_updates:1}\",\"status\":200}"}')"
+  -d "$FILE_DATA")"
 
 echo $RESPONSE
 
@@ -267,12 +263,12 @@ get_environment() {
 
 clean() {
 	# TODO: fix permissions issue
-	kubectl delete all --all -n $NAMESPACE
-	kubectl delete virtualservices.networking.istio.io --all -n $NAMESPACE
-	kubectl delete envoyfilters.networking.istio.io --all -n $NAMESPACE
-	kubectl delete destinationrules.networking.istio.io --all -n $NAMESPACE
-	kubectl delete gateways.networking.istio.io --all -n $NAMESPACE
-	kubectl delete serviceentries.networking.istio.io --all -n $NAMESPACE
+	kubectl delete all -n $NAMESPACE -l app=$APP_NAME
+	kubectl delete virtualservices.networking.istio.io -n $NAMESPACE -l app=$APP_NAME
+	kubectl delete envoyfilters.networking.istio.io -n $NAMESPACE -l app=$APP_NAME
+	kubectl delete destinationrules.networking.istio.io -n $NAMESPACE -l app=$APP_NAME
+	kubectl delete gateways.networking.istio.io -n $NAMESPACE -l app=$APP_NAME
+	kubectl delete serviceentries.networking.istio.io -n $NAMESPACE -l app=$APP_NAME
 	volumeMountsindex=$(kubectl get ds fluentd -n logging -o json | jq '.spec.template.spec.containers[0].volumeMounts[].name' | awk "/fluentd-$APP_NAME-conf-$NAMESPACE/{print NR-1}")
 	volumeindex=$(kubectl get ds fluentd -n logging -o json | jq '.spec.template.spec.volumes[].name' | awk "/fluentd-$APP_NAME-conf-$NAMESPACE/{print NR-1}")
 	# TODO: check that volumeMountsindex and volumeindex are not empty, otherwise
