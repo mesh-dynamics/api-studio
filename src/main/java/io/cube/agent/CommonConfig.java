b10e15de1efe641d6b515800b6847050df72d70a
Ravi Vijay <ravi.vijay@cubecorp.io>
2019-12-13 11:43:46 +0530

    Address minor comment

--- src/main/java/io/cube/agent/CommonConfig.java
+++ src/main/java/io/cube/agent/CommonConfig.java
@@ -1,5 +1,6 @@
 package io.cube.agent;
 
+import java.util.Optional;
 import java.util.Properties;
 
 import io.opentracing.Tracer;
@@ -25,7 +26,7 @@ public class CommonConfig {
 
     public String customerId, app, instance, serviceName;
 
-    public CommonConfig() {
+    public CommonConfig() throws Exception {
         properties = new java.util.Properties();
         try {
             properties.load(this.getClass().getClassLoader().
@@ -33,16 +34,26 @@ public class CommonConfig {
         } catch (Exception e) {
             LOGGER.error("Error while initializing config :: " + e.getMessage() );
         }
-        CUBE_RECORD_SERVICE_URI = fromEnvOrProperties(Constants.MD_RECORD_SERVICE_PROP);
-        CUBE_MOCK_SERVICE_URI = fromEnvOrProperties(Constants.MD_MOCK_SERVICE_PROP);
-        READ_TIMEOUT = Integer.parseInt(fromEnvOrProperties(Constants.MD_READ_TIMEOUT_PROP));
-        CONNECT_TIMEOUT = Integer.parseInt(fromEnvOrProperties(Constants.MD_CONNECT_TIMEOUT_PROP));
-        RETRIES = Integer.parseInt(fromEnvOrProperties(Constants.MD_RETRIES_PROP));
-        customerId =fromEnvOrProperties(Constants.MD_CUSTOMER_PROP);
-        app = fromEnvOrProperties(Constants.MD_APP_PROP);
-        instance = fromEnvOrProperties(Constants.MD_INSTANCE_PROP);
-        serviceName = fromEnvOrProperties(Constants.MD_SERVICE_PROP);
-        intent = fromEnvOrProperties(Constants.MD_INTENT_PROP);
+        CUBE_RECORD_SERVICE_URI = fromEnvOrProperties(Constants.MD_RECORD_SERVICE_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Record Endpoint Not Specified"));
+        CUBE_MOCK_SERVICE_URI = fromEnvOrProperties(Constants.MD_MOCK_SERVICE_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Mock Endpoint Not Specified"));
+        READ_TIMEOUT = Integer.parseInt(fromEnvOrProperties(Constants.MD_READ_TIMEOUT_PROP).
+            orElseThrow(() -> new Exception("Mesh-D Read Timeout Not Specified")));
+        CONNECT_TIMEOUT = Integer.parseInt(fromEnvOrProperties(Constants.MD_CONNECT_TIMEOUT_PROP).
+            orElseThrow(() -> new Exception("Mesh-D Connection Timeout Not Specified")));
+        RETRIES = Integer.parseInt(fromEnvOrProperties(Constants.MD_RETRIES_PROP).
+            orElseThrow(() -> new Exception("Mesh-D Connection Retry Limit Not Specified")));
+        customerId =fromEnvOrProperties(Constants.MD_CUSTOMER_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Customer Id Not Specified"));
+        app = fromEnvOrProperties(Constants.MD_APP_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D App Name Not Specified"));
+        instance = fromEnvOrProperties(Constants.MD_INSTANCE_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Instance Not Specified"));
+        serviceName = fromEnvOrProperties(Constants.MD_SERVICE_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Service Name Not Specified"));
+        intent = fromEnvOrProperties(Constants.MD_INTENT_PROP)
+            .orElseThrow(() -> new Exception("Mesh-D Intent Not Specified"));
 
         Tracer tracer = CommonUtils.init("tracer");
         try {
@@ -54,9 +65,9 @@ public class CommonConfig {
         LOGGER.info("CUBE MOCK SERVICE :: " + CUBE_MOCK_SERVICE_URI);
     }
 
-    private String fromEnvOrProperties(String propertyName) {
+    private Optional<String> fromEnvOrProperties(String propertyName) {
         return CommonUtils.fromEnvOrSystemProperties(propertyName)
-            .orElse(properties.getProperty(propertyName));
+            .or(() -> Optional.ofNullable(properties.getProperty(propertyName)));
     }
 
 
