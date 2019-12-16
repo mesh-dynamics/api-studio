f443dd6bc9bef7c45316fdae122e08efef9743ca
Aakash Singhal <aakash.singhal@cubecorp.io>
2019-12-16 12:54:11 +0530

    Modify replay URL

--- moviebooktest.sh
+++ moviebooktest.sh
@@ -4,7 +4,8 @@ set -x
 #Init Replay
 
 REPLAY_PATHS=minfo/listmovies,minfo/returnmovie,minfo/rentmovie,minfo/liststores
-ENDPOINT=http://demo.dev.cubecorp.io
+REPLAY_ENDPOINT=http://staging.dev.cubecorp.io
+CUBE_ENDPOINT=http://staging.dev.cubecorp.io
 INSTANCE_ID=prod
 USER_ID=demo@cubecorp.io
 REPLAY_PATHS=$(echo $REPLAY_PATHS | tr "," "\n")
@@ -14,10 +15,10 @@ do
 done
 
 REPLAY_PATHS=${TEMP_PATH::${#TEMP_PATH}-1}
-BODY="$REPLAY_PATHS&endPoint=$ENDPOINT&instanceId=$INSTANCE_ID&templateSetVer=DEFAULT&userId=$USER_ID"
+BODY="$REPLAY_PATHS&endPoint=$REPLAY_ENDPOINT&instanceId=$INSTANCE_ID&templateSetVer=DEFAULT&userId=$USER_ID"
 
 REPLAY_ID=$(curl -X POST \
-	http://demo.dev.cubecorp.io/rs/start/Recording--1834097741 \
+	$CUBE_ENDPOINT/rs/start/Recording--1834097741 \
 	-H 'Content-Type: application/x-www-form-urlencoded' \
 	-H 'cache-control: no-cache' \
 	-d $BODY \
@@ -34,13 +35,13 @@ echo $REPLAY_ID
 #Status Check
 COUNT=0
 while [ "$STATUS" != "Completed" ] && [ "$STATUS" != "Error" ] && [ "$COUNT" != "20" ]; do
-	STATUS=$(curl -X GET http://demo.dev.cubecorp.io/rs/status/CubeCorp/MovieInfo/d2b10037-c6be-4fe8-b3ea-fb44213e922e/$REPLAY_ID | sed 's/^.*"status":"\([^"]*\)".*/\1/')
+	STATUS=$(curl -X GET $CUBE_ENDPOINT/rs/status/CubeCorp/MovieInfo/d2b10037-c6be-4fe8-b3ea-fb44213e922e/$REPLAY_ID | sed 's/^.*"status":"\([^"]*\)".*/\1/')
 	sleep 5
 	COUNT=$((COUNT+1))
 done
 
 #Run analyze
-ANALYZE=$(curl -X POST http://demo.dev.cubecorp.io/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
+ANALYZE=$(curl -X POST $CUBE_ENDPOINT/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H 'cache-control: no-cache')
 REQNOTMATCHED=$(echo $ANALYZE | sed 's/^.*"reqnotmatched":\([^"]*\).*/\1/' | cut -d ',' -f 1)
 RESPNOTMATCHED=$(echo $ANALYZE | sed 's/^.*"respnotmatched":\([^"]*\).*/\1/' | cut -d ',' -f 1)
 
