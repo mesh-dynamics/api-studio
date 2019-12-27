cccc7866ec3eb07ce2f04783116a03d4bc777b06
Ravi Vijay <ravi.vijay@cubecorp.io>
2019-12-27 14:00:03 +0530

    Remove unused function

--- src/main/java/io/md/core/ReplayTypeEnum.java
+++ src/main/java/io/md/core/ReplayTypeEnum.java
@@ -4,22 +4,4 @@ public enum ReplayTypeEnum {
 	HTTP,
 	THRIFT,
 	GRPC;
-
-
-	public static ReplayTypeEnum fromString(String replayType) {
-		// Default
-		if (replayType == null) {
-			return HTTP;
-		}
-
-		switch (replayType.toUpperCase()) {
-			case "THRIFT":
-				return THRIFT;
-			case "GRPC":
-				return GRPC;
-			case "HTTP":
-			default:
-				return HTTP;
-		}
-	}
 }
