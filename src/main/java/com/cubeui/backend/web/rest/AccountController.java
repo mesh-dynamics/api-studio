354f7a3b67722ced78e295646a5ac3c7af970372
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2020-01-13 20:21:39 +0530

    add email domain check to create account flow

--- src/main/java/com/cubeui/backend/web/rest/AccountController.java
+++ src/main/java/com/cubeui/backend/web/rest/AccountController.java
@@ -3,12 +3,16 @@ package com.cubeui.backend.web.rest;
 import com.cubeui.backend.domain.DTO.ChangePasswordDTO;
 import com.cubeui.backend.domain.DTO.KeyAndPasswordDTO;
 import com.cubeui.backend.domain.DTO.UserDTO;
+import com.cubeui.backend.domain.EmailDomain;
 import com.cubeui.backend.domain.User;
+import com.cubeui.backend.repository.EmailDomainRepository;
 import com.cubeui.backend.service.MailService;
 import com.cubeui.backend.service.UserService;
 import com.cubeui.backend.web.exception.DuplicateRecordException;
 import com.cubeui.backend.web.exception.InvalidDataException;
 import com.cubeui.backend.web.exception.RecordNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -34,27 +38,57 @@ public class AccountController {
 
     private UserService userService;
     private MailService mailService;
-
-    public AccountController(UserService userService, MailService mailService) {
+    private EmailDomainRepository emailDomainRepository;
+    
+    public AccountController(UserService userService,
+        MailService mailService, EmailDomainRepository emailDomainRepository) {
         this.userService = userService;
         this.mailService = mailService;
+        this.emailDomainRepository = emailDomainRepository;
+    }
+
+    Optional<EmailDomain> validateEmailDomain(String email) {
+        // todo validate email string
+        String[] emailSplit = email.split("@");
+        String domain = emailSplit[1];
+        Optional<EmailDomain> emailDomain = emailDomainRepository.findByDomain(domain);
+        return emailDomain;
     }
 
     @PostMapping("/create-user")
     public ResponseEntity createUser(@RequestBody UserDTO userDTO, HttpServletRequest request) {
-        Optional<User> optionalUser = this.userService.getByUsername(userDTO.getEmail());
-        if (optionalUser.isPresent()) {
-            throw new DuplicateRecordException("User with username '" + optionalUser.get().getUsername() + "' already exists.");
+        Optional<User> existingUser = this.userService.getByUsername(userDTO.getEmail());
+        // check existing user
+        if (existingUser.isPresent()) {
+            throw new DuplicateRecordException("User with username '"
+                + existingUser.get().getUsername() + "' already exists.");
         } else {
-            User saved = this.userService.save(userDTO, false);
-            mailService.sendActivationEmail(saved);
-            return created(
-                    ServletUriComponentsBuilder
-                            .fromContextPath(request)
-                            .path("/api/users/{id}")
-                            .buildAndExpand(saved.getId())
-                            .toUri())
-                    .body(saved);
+            // validate email domain and set customer id from email
+            Optional<EmailDomain> emailDomain = validateEmailDomain(userDTO.getEmail());
+            if (emailDomain.isPresent()) {
+                EmailDomain e = emailDomain.get();
+                userDTO.setCustomerId(e.getCustomer().getId());
+
+                // set default roles
+                List<String> defaultRoles = Arrays.asList("ROLE_USER");
+                userDTO.setRoles(defaultRoles);
+
+                // save user
+                User saved = this.userService.save(userDTO, false);
+
+                // send activation mail
+                mailService.sendActivationEmail(saved);
+
+                return created(
+                        ServletUriComponentsBuilder
+                                .fromContextPath(request)
+                                .path("/api/users/{id}")
+                                .buildAndExpand(saved.getId())
+                                .toUri())
+                        .body(saved);
+            } else {
+                throw new InvalidDataException("Invalid email");
+            }
         }
     }
 
