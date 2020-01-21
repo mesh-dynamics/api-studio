41089a0584c402311ac02bdfa742f8f4add1b296
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2020-01-21 18:42:05 +0530

    simplify recaptcha code

--- src/main/java/com/cubeui/backend/service/ReCaptchaAPIService.java
+++ src/main/java/com/cubeui/backend/service/ReCaptchaAPIService.java
@@ -9,6 +9,9 @@ import com.cubeui.backend.service.exception.ReCaptchaInvalidException;
 import java.net.URI;
 import java.util.regex.Pattern;
 import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.Logger.Level;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +20,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional
-@Log
+@Slf4j
 public class ReCaptchaAPIService {
 
     @Value("${external.recaptcha.secret-key}")
     private String reCaptchaSecret;
+
+    @Value("${external.recaptcha.base-url}")
+    private String reCaptchaBaseURL;
 
     private RestTemplate restTemplate;
 
@@ -34,18 +40,20 @@ public class ReCaptchaAPIService {
 
     public void processResponse(String response, String clientIPAddress) {
         if(!responseSanityCheck(response)) {
+            log.error("Response contains invalid characters");
             throw new InvalidReCaptchaException("Response contains invalid characters");
         }
 
-        URI verifyUri = URI.create(String.format(
-            "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s",
-                reCaptchaSecret, response, clientIPAddress));
+        URI verifyUri = URI.create(String.format("%s?secret=%s&response=%s&remoteip=%s",
+                reCaptchaBaseURL, reCaptchaSecret, response, clientIPAddress));
 
-        GoogleRecaptchaResponseDTO googleResponse = restTemplate.getForObject(verifyUri, GoogleRecaptchaResponseDTO.class);
-
+        GoogleRecaptchaResponseDTO googleResponse
+            = restTemplate.getForObject(verifyUri, GoogleRecaptchaResponseDTO.class);
         if(!googleResponse.isSuccess()) {
+            log.error("reCaptcha validation error occurred: " + googleResponse.getErrorCodes());
             throw new ReCaptchaInvalidException("reCaptcha was not successfully validated");
         }
+        log.info("ReCaptcha validated successfully");
     }
 
     private boolean responseSanityCheck(String response) {
--- src/main/java/com/cubeui/backend/service/dto/GoogleRecaptchaResponseDTO.java
+++ src/main/java/com/cubeui/backend/service/dto/GoogleRecaptchaResponseDTO.java
@@ -14,12 +14,6 @@ import lombok.Setter;
 // from https://www.baeldung.com/spring-security-registration-captcha
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonPropertyOrder({
-    "success",
-    "challenge_ts",
-    "hostname",
-    "error-codes"
-})
 @Getter
 @Setter
 public class GoogleRecaptchaResponseDTO {
@@ -34,40 +28,5 @@ public class GoogleRecaptchaResponseDTO {
   private String hostname;
 
   @JsonProperty("error-codes")
-  private ErrorCode[] errorCodes;
-
-  @JsonIgnore
-  public boolean hasClientError() {
-    ErrorCode[] errors = getErrorCodes();
-    if(errors == null) {
-      return false;
-    }
-    for(ErrorCode error : errors) {
-      switch(error) {
-        case InvalidResponse:
-        case MissingResponse:
-          return true;
-      }
-    }
-    return false;
-  }
-
-  enum ErrorCode {
-    MissingSecret,     InvalidSecret,
-    MissingResponse,   InvalidResponse;
-
-    private static Map<String, ErrorCode> errorsMap = new HashMap<String, ErrorCode>(4);
-
-    static {
-      errorsMap.put("missing-input-secret",   MissingSecret);
-      errorsMap.put("invalid-input-secret",   InvalidSecret);
-      errorsMap.put("missing-input-response", MissingResponse);
-      errorsMap.put("invalid-input-response", InvalidResponse);
-    }
-
-    @JsonCreator
-    public static ErrorCode forValue(String value) {
-      return errorsMap.get(value.toLowerCase());
-    }
-  }
+  private String[] errorCodes;
 }
