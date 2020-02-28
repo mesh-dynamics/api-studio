bbeeffa17731c444ef79888434be5f6db1b01600
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2020-02-28 14:49:29 +0530

    remove _old

--- src/common/routes/_old/diff_results/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Route } from 'react-router'
-import DiffResults from "./DiffResults";
-
-export default [
-    <Route exact key="DiffResults" path="/diff_results" component={DiffResults} />,
-]
