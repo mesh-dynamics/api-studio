fbd07f006ee5b0bad15f3f8c781b0e69edc4116f
Siddhant Mutha <siddhant.mutha@cubecorp.io>
2020-03-09 13:36:32 +0530

    use env var in config

--- src/common/config.js
+++ src/common/config.js
@@ -1,8 +1,8 @@
 const config = {
-    apiBaseUrl: "https://demo.dev.cubecorp.io/api",
-    recordBaseUrl: "https://demo.dev.cubecorp.io/api/cs",
-    replayBaseUrl: "https://demo.dev.cubecorp.io/api/rs",
-    analyzeBaseUrl: "https://demo.dev.cubecorp.io/api/as",
+    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "/api",
+    recordBaseUrl: process.env.REACT_APP_RECORD_BASE_URL || "/api/cs",
+    replayBaseUrl: process.env.REACT_APP_REPLAY_BASE_URL || "/api/rs",
+    analyzeBaseUrl: process.env.REACT_APP_ANALYZE_BASE_URL || "/api/as",
     defaultPageSize: process.env.REACT_APP_DEFAULT_PAGE_SIZE || 5,
 };
 
