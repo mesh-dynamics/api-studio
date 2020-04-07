26140b30386f82594a2ce6eecb575c276a55d30d
Abdul Quyoom <abdulquyoombhat@gmail.com>
2020-04-07 17:06:53 +0530

    update imports

--- MIRest/src/com/cubeiosample/webservices/rest/jersey/MovieRentalRest.java
+++ MIRest/src/com/cubeiosample/webservices/rest/jersey/MovieRentalRest.java
@@ -1,7 +1,14 @@
 package com.cubeiosample.webservices.rest.jersey;
 // TODO: change the package name to com.cubeio.samples.MIRest
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
