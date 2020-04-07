f0dc0ab3ef882b647ff95c9746d664a78bdfce20
Ravi Vijay <ravi.vijay@meshdynamics.io>
2020-04-07 12:12:24 +0530

    Removing functions which are not required

--- src/main/java/io/cube/agent/SimpleMocker.java
+++ src/main/java/io/cube/agent/SimpleMocker.java
@@ -51,87 +51,6 @@ public class SimpleMocker implements Mocker {
 		this.gson = gson;
 	}
 
-
-	//TODO: Cleanup - phase this out
-	@Override
-	public FnResponseObj mockOld(FnKey fnKey, Optional<String> traceId, Optional<String> spanId,
-		Optional<String> parentSpanId,
-		Optional<Instant> prevRespTS, Optional<Type> retType, Object... args) {
-
-		try {
-			String[] argVals =
-				Arrays.stream(args).map(rethrowFunction(gson::toJson)).toArray(String[]::new);
-			Integer[] argsHash = Arrays.stream(argVals).map(String::hashCode)
-				.toArray(Integer[]::new);
-			String traceIdString = traceId.orElse("N/A");
-			LOGGER.info(new ObjectMessage(
-				Map.of("state", "Before Mock", "func_name", fnKey.fnName, "trace_id",
-					traceIdString)));
-			var counter = new Object() {
-				int x = 0;
-			};
-			Arrays.stream(argVals)
-				.forEach(arg -> LOGGER.info(new ObjectMessage(Map.of("func_name", fnKey.fnName
-					, "trace_id", traceIdString, "arg_hash", argsHash[counter.x],
-					"arg_val_" + counter.x++, arg))));
-
-			//This key is to identify cases where multiple Solr docs are matched
-			Integer key = traceId.orElse("").concat(spanId.orElse(""))
-				.concat(parentSpanId.orElse(""))
-				.concat(fnKey.signature).hashCode();
-
-			// forming a dummy req response object with empty ret value, we can just serialize this object
-			// and send it to cube mock service to get appropriate response
-			FnReqResponse fnReqResponse = new FnReqResponse(fnKey.customerId, fnKey.app,
-				fnKey.instanceId,
-				fnKey.service, fnKey.fnSigatureHash, fnKey.fnName, traceId, spanId, parentSpanId,
-				Optional.ofNullable(prevRespTS.orElse(fnMap.get(key))), argsHash, argVals, "",
-				RetStatus.Success, Optional.empty());
-
-			Optional<FnResponse> ret = cubeClient.getMockResponse(fnReqResponse);
-
-			// need to check is before trying to convert return value, otherwise null return value also leads to
-			// empty optional
-			if (ret.isEmpty()) {
-				LOGGER.error(new ObjectMessage(Map.of("reason", "No Matching Response Received"
-					, "trace_id", traceIdString, "func_name", fnKey.fnName)));
-				return new FnResponseObj(null, Optional.empty(), RetStatus.Success,
-					Optional.empty());
-			} else {
-				FnResponse response = ret.get();
-				LOGGER.info(new ObjectMessage(
-					Map.of("state", "After Mock", "func_name", fnKey.fnName, "trace_id",
-						traceIdString,
-						"return_val", response.retVal)));
-				//If multiple Solr docs were returned, we need to maintain the last timestamp
-				//to be used in the next mock call.
-				if (response.retStatus == RetStatus.Success) {
-					if (response.multipleResults) {
-						fnMap.put(key, response.timeStamp.get());
-					} else {
-						fnMap.remove(key); //remove if the key is present.
-					}
-				} else {
-					fnMap.remove(key);
-				}
-
-				Object retOrExceptionVal = gson.fromJson(response.retVal,
-					retType.isPresent() ? retType.get() : getRetOrExceptionClass(response,
-						fnKey.function.getGenericReturnType()));
-				return new FnResponseObj(retOrExceptionVal, response.timeStamp, response.retStatus,
-					response.exceptionType);
-			}
-
-		} catch (Throwable e) {
-			// encode can throw UnsupportedEncodingException
-			String stackTraceError = UtilException
-				.extractFirstStackTraceLocation(e.getStackTrace());
-			LOGGER.error(new ObjectMessage(Map.of("func_name", fnKey.fnName, "trace_id", traceId)),
-				e);
-			return new FnResponseObj(null, Optional.empty(), RetStatus.Success, Optional.empty());
-		}
-	}
-
 	@Override
 	public FnResponseObj mock(FnKey fnKey, Optional<String> traceId, Optional<String> spanId,
 		Optional<String> parentSpanId,
@@ -166,8 +85,6 @@ public class SimpleMocker implements Mocker {
 						retType.isPresent() ? retType.get() : getRetOrExceptionClass(resp,
 							fnKey.function.getGenericReturnType()));
 				} catch (Exception e) {
-					String stackTraceError = UtilException
-						.extractFirstStackTraceLocation(e.getStackTrace());
 					LOGGER.error(new ObjectMessage(Map.of("func_signature", eve.apiPath, "trace_id",
 						eve.getTraceId())), e);
 					return new FnResponseObj(null, Optional.empty(), RetStatus.Success,
