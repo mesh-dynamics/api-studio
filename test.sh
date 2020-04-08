325a212a7567300266f826373df92921e176e133
Aakash Singhal <ethicalaakash@gmail.com>
2020-04-08 09:58:50 +0530

    Add test script

---
+++ test.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+record() {
+  RECORDING_ID=$(curl -X POST $CUBE_ENDPOINT/api/cs/start/CubeCorp/jersey_1_19_demo/prod/jersy119-$DRONE_BUILD_NUMBER/default \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H "Authorization: Bearer $AUTH_TOKEN" \
+  -H 'cache-control: no-cache' \
+  -d "name=jersy119-$DRONE_BUILD_NUMBER&userId=CubeCorp" | jq .id | tr -d '"')
+  echo $RECORDING_ID
+}
+
+generate_traffic() {
+  for ((i=1;i<=$1;i++)); do
+    curl -f --location --request POST 'http://jersy119.dev.cubecorp.io:8080/emp/emp/getEmp' \
+    --header 'Content-Type: application/xml' \
+    --data-raw '<empRequest><id>1</id><name>PK</name></empRequest>'
+  done
+}
+
+stop_recording() {
+  curl -X POST https://demo.dev.cubecorp.io/api/cs/stop/$RECORDING_ID \
+  -H "Authorization: Bearer $AUTH_TOKEN"
+}
+
+replay() {
+  BODY="endPoint=$REPLAY_ENDPOINT&instanceId=$INSTANCE_ID&templateSetVer=$TEMPLATE&userId=$USER_ID"
+  REPLAY_ID=$(curl -X POST \
+		$CUBE_ENDPOINT/api/rs/start/$RECORDING_ID \
+		-H 'Content-Type: application/x-www-form-urlencoded' \
+		-H "Authorization: Bearer $AUTH_TOKEN" \
+		-H 'cache-control: no-cache' \
+		-d $BODY | jq .replayId | tr -d '"')
+  COUNT=0
+  while [ "$STATUS" != "Completed" ] && [ "$STATUS" != "Error" ] && [ "$COUNT" != "30" ]; do
+    STATUS=$(curl -X GET $CUBE_ENDPOINT/api/rs/status/$REPLAY_ID -H "Authorization: Bearer $AUTH_TOKEN" | jq .status | tr -d '"')
+    sleep 10
+    COUNT=$((COUNT+1))
+  done
+}
+analyze() {
+  ANALYZE=$(curl -X POST $CUBE_ENDPOINT/api/as/analyze/$REPLAY_ID -H 'Content-Type: application/x-www-form-urlencoded' -H "Authorization: Bearer $AUTH_TOKEN" -H 'cache-control: no-cache')
+  REQCOUNT=$(echo $ANALYZE | jq .reqCnt )
+  RESPNOTMATCHED=$(echo $ANALYZE | jq .respNotMatched )
+
+  #Display replay ID
+  echo "Replay ID:" $REPLAY_ID
+  #Exit with non-zero exit code if reqstnotmatched and respnotmatchted are have nono-zero value
+  if [ "$RESPNOTMATCHED" = "0" ] && [ "$REQCOUNT" != "0" ]; then
+    TEST_STATUS="test passed"
+    EXIT_CODE=0
+  else
+    TEST_STATUS="test failed"
+    EXIT_CODE=1
+  fi
+}
+main() {
+  set -x
+  apk add jq
+  CUBE_ENDPOINT=https://demo.dev.cubecorp.io
+  DRONE_BUILD_NUMBER=16
+  TEMPLATE=DEFAULT
+  USER_ID=CubeCorp
+  REPLAY_ENDPOINT=http://jersy119.dev.cubecorp.io
+  INSTANCE_ID=prod
+  AUTH_TOKEN="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNZXNoREFnZW50VXNlckBtZXNoZHluYW1pY3MuaW8iLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwidHlwZSI6InBhdCIsImN1c3RvbWVyX2lkIjoxLCJpYXQiOjE1ODI4ODE2MjgsImV4cCI6MTg5ODI0MTYyOH0.P4DAjXyODV8cFPgObaULjAMPg-7xSbUsVJ8Ohp7xTQI"
+  record
+  generate_traffic 5
+  stop_recording
+  sleep 20
+  replay
+  analyze
+  echo "Replay ID:" $REPLAY_ID
+	echo $TEST_STATUS
+  exit $EXIT_CODE
+}
+main "$@"
