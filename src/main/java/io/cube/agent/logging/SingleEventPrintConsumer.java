6499257b9347d1619553685e86191b72d3626e9c
Ravi Vijay <ravi.vijay@cubecorp.io>
2020-05-25 17:20:21 +0530

    Use Minimal Pretty Print with Generator

--- src/main/java/io/cube/agent/logging/SingleEventPrintConsumer.java
+++ src/main/java/io/cube/agent/logging/SingleEventPrintConsumer.java
@@ -1,8 +1,8 @@
 package io.cube.agent.logging;
 
 
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 import org.clapper.util.io.RollingFileWriter;
@@ -10,7 +10,11 @@ import org.clapper.util.io.RollingFileWriter.Compression;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.lmax.disruptor.EventHandler;
 
 import io.cube.agent.CommonConfig;
@@ -21,12 +25,12 @@ public class SingleEventPrintConsumer {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SingleEventPrintConsumer.class);
 
 
-	private ObjectMapper jsonMapper = CubeObjectMapperProvider.getInstance();
-
-	private PrintWriter eventWriter;
+	private ObjectWriter objectWriter;
+	private JsonGenerator generator;
 
 	public SingleEventPrintConsumer() {
 		System.out.println("SingleEventPrintConsumer Constructor Called");
+		PrintWriter eventWriter;
 		if (CommonConfig.getInstance().disruptorOutputLocation.equals("stdout")) {
 			eventWriter = new PrintWriter(System.out);
 		} else {
@@ -41,13 +45,22 @@ public class SingleEventPrintConsumer {
 				LOGGER.debug("Unable to find outstream file. Setting outstream as nullOutputStream ", e);
 			}
 		}
+		try {
+			ObjectMapper jsonMapper = CubeObjectMapperProvider.getInstance();
+			objectWriter = jsonMapper.writer();
+			generator = new JsonFactory().createGenerator(eventWriter);
+			generator.setPrettyPrinter(new MinimalPrettyPrinter("\n"));
+		} catch (IOException e) {
+			LOGGER.error("Unable to initialize json generator" ,e);
+			throw new RuntimeException(e);
+		}
+
 	}
 
 	public EventHandler<ValueEvent> getEventHandler() {
 		return (event, sequence, endOfBatch)
 			-> {
-			jsonMapper.writeValue(eventWriter, event);
-			eventWriter.println();
+			objectWriter.writeValue(generator, event);
 		};
 	}
 
