68c46af8e882dc11ee4458335fa995fa85b6fb8a
muthumani <muthumani@muthumanis-MacBook-Pro.local>
2020-05-26 21:47:21 +0530

    dogfooding support for devtool

--- src/main/java/com/cubeui/backend/BackendApplication.java
+++ src/main/java/com/cubeui/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package com.cubeui.backend;
 
+import io.md.cube.spring.egress.RestTemplateMockInterceptor;
+import io.md.cube.spring.egress.RestTemplateTracingInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,7 +27,7 @@ import static com.cubeui.backend.security.Constants.SPRING_PROFILE_DEVELOPMENT;
 
 @Slf4j
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cubeui.backend", "io.md.cube"})
 public class BackendApplication {
 
     public static void main(String[] args) {
@@ -53,7 +57,12 @@ public class BackendApplication {
 
     @Bean
     public RestTemplate getRestTemplate() {
-      return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        ArrayList<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new RestTemplateMockInterceptor());
+        interceptors.add(new RestTemplateTracingInterceptor());
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
     }
 
     private static void logApplicationStartup(Environment env) {
