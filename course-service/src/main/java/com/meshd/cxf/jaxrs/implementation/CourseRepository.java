a291a6f461d56b32321819e01bcfe8edcc697918
Paawan <17042021+paawan01@users.noreply.github.com>
2020-06-02 00:54:07 +0530

    Add mocking filter to apis

--- course-service/src/main/java/com/meshd/cxf/jaxrs/implementation/CourseRepository.java
+++ course-service/src/main/java/com/meshd/cxf/jaxrs/implementation/CourseRepository.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -25,55 +24,57 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.cube.apachecxf.egress.MDClientLoggingFilter;
+import io.cube.apachecxf.egress.MDClientMockingFilter;
 import io.cube.apachecxf.egress.MDClientTracingFilter;
 
 @Path("meshd")
 @Produces("application/json")
 public class CourseRepository {
-    ObjectMapper objectMapper = new ObjectMapper();
-    private Map<Integer, Course> courses = new HashMap<>();
-    private String BASE_URL = System.getenv("student.service.url");
-    private String URL = BASE_URL!=null ? BASE_URL + "/meshd/students?source=aaa&trial=bbb" :
-        "http://34.220.106.159:8080/meshd/students?source=aaa&trial=bbb";
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	private Map<Integer, Course> courses = new HashMap<>();
+	private String BASE_URL = System.getenv("student.service.url");
+	private String URL = BASE_URL != null ? BASE_URL + "/meshd/students?source=aaa&trial=bbb" :
+		"http://34.220.106.159:8080/meshd/students?source=aaa&trial=bbb";
 //    private String URL = "http://34.220.106.159:8080/meshd/students?source=aaa&trial=bbb";
 
-    {
-        List<Integer> studentIds = new ArrayList<>();
-        studentIds.add(1);
-        studentIds.add(2);
-        studentIds.add(2);
+	{
+		List<Integer> studentIds = new ArrayList<>();
+		studentIds.add(1);
+		studentIds.add(2);
+		studentIds.add(2);
 
-        Course course1 = new Course();
-        Course course2 = new Course();
-        course1.setId(1);
-        course1.setName("REST with Spring");
-        course1.setStudents(studentIds);
-        course2.setId(2);
-        course2.setName("Learn Spring Security");
+		Course course1 = new Course();
+		Course course2 = new Course();
+		course1.setId(1);
+		course1.setName("REST with Spring");
+		course1.setStudents(studentIds);
+		course2.setId(2);
+		course2.setName("Learn Spring Security");
 
-        courses.put(1, course1);
-        courses.put(2, course2);
-    }
+		courses.put(1, course1);
+		courses.put(2, course2);
+	}
 
-    @GET
-    @Path("courses/{courseId}")
-    public Course getCourse(@PathParam("courseId") int courseId) {
-        return findById(courseId);
-    }
+	@GET
+	@Path("courses/{courseId}")
+	public Course getCourse(@PathParam("courseId") int courseId) {
+		return findById(courseId);
+	}
 
-    @PUT
-    @Path("courses/{courseId}")
-    public Response updateCourse(@PathParam("courseId") int courseId, Course course) {
-        Course existingCourse = findById(courseId);
-        if (existingCourse == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        if (existingCourse.equals(course)) {
-            return Response.notModified().build();
-        }
-        courses.put(courseId, course);
-        return Response.ok().build();
-    }
+	@PUT
+	@Path("courses/{courseId}")
+	public Response updateCourse(@PathParam("courseId") int courseId, Course course) {
+		Course existingCourse = findById(courseId);
+		if (existingCourse == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
+		if (existingCourse.equals(course)) {
+			return Response.notModified().build();
+		}
+		courses.put(courseId, course);
+		return Response.ok().build();
+	}
 
 //    @POST
 //    @Path("courses")
@@ -87,94 +88,93 @@ public class CourseRepository {
 //        return Response.ok(course).build();
 //    }
 
-    @POST
-    @Path("courses/{courseId}/student")
-    public Response addStudent(@PathParam("courseId") int courseId, Student student) {
-        Course existingCourse = findById(courseId);
-        if (existingCourse == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
+	@POST
+	@Path("courses/{courseId}/student")
+	public Response addStudent(@PathParam("courseId") int courseId, Student student) {
+		Course existingCourse = findById(courseId);
+		if (existingCourse == null) {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 
-        try {
-            return existingCourse.createStudent(student);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			return existingCourse.createStudent(student);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return Response.serverError().build();
-    }
+		return Response.serverError().build();
+	}
 
-    @Path("courses/{courseId}/students")
-    public Course pathToStudent(@PathParam("courseId") int courseId) {
-        return findById(courseId);
-    }
+	@Path("courses/{courseId}/students")
+	public Course pathToStudent(@PathParam("courseId") int courseId) {
+		return findById(courseId);
+	}
 
-    private Course findById(int id) {
-        for (Map.Entry<Integer, Course> course : courses.entrySet()) {
-            if (course.getKey() == id) {
-                return course.getValue();
-            }
-        }
-        return null;
-    }
+	private Course findById(int id) {
+		for (Map.Entry<Integer, Course> course : courses.entrySet()) {
+			if (course.getKey() == id) {
+				return course.getValue();
+			}
+		}
+		return null;
+	}
 
-    @GET
-    @Path("courses/students/{studentId}")
-    public Student findStudentById(@PathParam("studentId") int id) throws URISyntaxException {
-//        WebClient studentWebClient = webClient.path(URL + id);
-        URIBuilder uriBuilder = new URIBuilder(URL);
-        uriBuilder.setPath(uriBuilder.getPath()+"/"+id);
-        WebClient studentWebClient = WebClient.create(uriBuilder.build().toString(), Arrays.asList(new MDClientLoggingFilter(), new MDClientTracingFilter())).accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).type(
-            javax.ws.rs.core.MediaType.APPLICATION_JSON);
-//        WebClient studentWebClient = webClient;
+	@GET
+	@Path("courses/students/{studentId}")
+	public Student findStudentById(@PathParam("studentId") int id) throws URISyntaxException {
+		URIBuilder uriBuilder = new URIBuilder(URL);
+		uriBuilder.setPath(uriBuilder.getPath() + "/" + id);
+		WebClient studentWebClient = WebClient.create(uriBuilder.build().toString(),
+			Arrays.asList(new MDClientLoggingFilter(), new MDClientTracingFilter(),
+				new MDClientMockingFilter()))
+			.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).type(
+				javax.ws.rs.core.MediaType.APPLICATION_JSON);
 
-        Response response = studentWebClient.get();
-        int code = response.getStatus();
-        if (code >= 200 && code <= 299) {
-            String studentString = response.readEntity(String.class);
-            Student student = null;
-            try {
-                student = objectMapper.readValue(studentString, Student.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return student;
+		Response response = studentWebClient.get();
+		int code = response.getStatus();
+		if (code >= 200 && code <= 299) {
+			String studentString = response.readEntity(String.class);
+			Student student = null;
+			try {
+				student = objectMapper.readValue(studentString, Student.class);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return student;
 //                return objectMapper.readValue(response.body().string(), Student.class);
-        }
-        return null;
-    }
+		}
+		return null;
+	}
 
-    @GET
-    @Path("/dummyCourseList")
-    public List<Course> dummyCourseList(@QueryParam("count") int courseCount) {
-        List<Course> courseList = new ArrayList<>();
-        Course course = new Course();
-        for (int i=0; i<courseCount; i++) {
-            course.setId(courseCount);
-            course.setName("Dummy Course");
-//            List<Integer> studentIds = new ArrayList<>();
-//            studentIds.add(i);
-//            course.setStudents(studentIds);
-            courseList.add(course);
-        }
-        return courseList;
-    }
+	@GET
+	@Path("/dummyCourseList")
+	public List<Course> dummyCourseList(@QueryParam("count") int courseCount) {
+		List<Course> courseList = new ArrayList<>();
+		Course course = new Course();
+		course.setId(courseCount);
+		course.setName("Dummy Course");
+		for (int i = 0; i < courseCount; i++) {
+			courseList.add(course);
+		}
+		return courseList;
+	}
 
-    @GET
-    @Path("/dummyStudentList")
-    public Response dummyStudentList(@QueryParam("count") int studentCount)
-        throws URISyntaxException {
-        URIBuilder uriBuilder = new URIBuilder(URL);
-        uriBuilder.setPath(uriBuilder.getPath()+"/dummyStudentList");
-        uriBuilder.addParameter("count", String.valueOf(studentCount));
-        WebClient studentWebClient = WebClient.create(uriBuilder.build().toString(), Arrays
-            .asList(new MDClientLoggingFilter(), new MDClientTracingFilter())).accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).type(
-            javax.ws.rs.core.MediaType.APPLICATION_JSON);
-//        WebClient studentWebClient = webClient;
+	@GET
+	@Path("/dummyStudentList")
+	public Response dummyStudentList(@QueryParam("count") int studentCount)
+		throws URISyntaxException {
+		URIBuilder uriBuilder = new URIBuilder(URL);
+		uriBuilder.setPath(uriBuilder.getPath() + "/dummyStudentList");
+		uriBuilder.addParameter("count", String.valueOf(studentCount));
+		WebClient studentWebClient = WebClient.create(uriBuilder.build().toString(), Arrays
+			.asList(new MDClientLoggingFilter(), new MDClientTracingFilter(),
+				new MDClientMockingFilter()))
+			.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).type(
+				javax.ws.rs.core.MediaType.APPLICATION_JSON);
 
-        Response response = studentWebClient.get();
-        return response;
-    }
+		Response response = studentWebClient.get();
+		return response;
+	}
 }
