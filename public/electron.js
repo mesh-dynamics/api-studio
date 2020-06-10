09608f9fa36bd5901a9d783c319c25f521b2a533
Samuel Marandi <samuel@91social.com>
2020-06-11 03:38:14 +0530

    working progress info

--- public/electron.js
+++ public/electron.js
@@ -59,107 +59,37 @@ ipcMain.on('app_version', (event) => {
     event.sender.send('app_version', { version: app.getVersion() });
 })
 
-autoUpdater.on('update-available', (event) => {
-    logger.info("Update available:::::::::::::")
-    logger.info("Logging event", event);
+autoUpdater.on('update-available', () => {
+    logger.info("Update available")
     mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-not-available', arg => {
-    logger.info('Update no available:::::::::::::');
+    logger.info('Update not available');
     logger.info(arg);
 });
 
-autoUpdater.on('download-progress', (event, arg) => {
-    logger.info('Download is in progress:::::::::::::');
-    logger.info('Printing Event::::', event);
-    logger.info('Printing Arg::::::', arg);
+autoUpdater.on('download-progress', (arg) => {
+    logger.info('Download is in progress...');
+    logger.info(arg)
     mainWindow.webContents.send('download_progress', Math.round(arg.percent));
 });
 
 autoUpdater.on('error', error => {
-    logger.info('An error has occured:::::::::::::');
+    logger.info('An error has occured::');
     logger.info(error.message);
     logger.info(error.stack);
 });
 
 autoUpdater.on('update-downloaded', info => {
     mainWindow.webContents.send('update_available');
-    logger.info("Update downloaded:::::::::::::")
+    logger.info("Update downloaded... Proceeding to install")
     const quitAndInstalled = autoUpdater.quitAndInstall();
-    logger.info('Installed new version:::::::::::::');
+    logger.info('Installed a new version');
     logger.info(quitAndInstalled);
 })
 
 ipcMain.on('restart_app', () => {
-    logger.info("Doing a quit and install:::::::::::::");
+    logger.info("Performing a quit and install");
     autoUpdater.quitAndInstall();
 });
-
-// const electron = require('electron')
-// const path = require('path')
-// const isDev = require('electron-is-dev')
-// const { autoUpdater } = require('electron-updater')
-// const app = electron.app
-// const BrowserWindow = electron.BrowserWindow
-// const ipcMain = electron.ipcMain
-
-// let mainWindow
-
-// const assetsPath = app.isPackaged ? path.join(process.resourcesPath, "assets") : "assets";
-
-// function createWindow() {
-//     mainWindow = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         webPreferences: {
-//         nodeIntegration: true,
-//         },
-//     })
-
-// mainWindow.loadURL(
-//     isDev
-//         ? 'http://localhost:3006'
-//         : `file://${path.join(__dirname, '../build/index.html')}`,
-//     )
-
-// mainWindow.on('closed', () => { mainWindow = null })
-
-// mainWindow.once('ready-to-show', () => {
-//         autoUpdater.checkForUpdatesAndNotify();
-//     });
-// }
-
-// app.on('ready', createWindow)
-
-// app.on('window-all-closed', () => {
-//     if (process.platform !== 'darwin') {
-//         app.quit()
-//     }
-// })
-
-// app.on('activate', () => {
-//     if (mainWindow === null) {
-//         createWindow()
-//     }
-// })
-
-// ipcMain.on('app_version', (event) => {
-//     event.sender.send('app_version', { version: app.getVersion() });
-// });
-
-// autoUpdater.on('update-available', () => {
-//     mainWindow.webContents.send('update_available');
-// });
-
-// autoUpdater.on('update-downloaded', () => {
-//     mainWindow.webContents.send('update_downloaded');
-// });
-
-// autoUpdater.on('error', (error) => {
-//     console.log('Error on update', error);
-// })
-
-// ipcMain.on('restart_app', () => {ipcMain
-//     autoUpdater.quitAndInstall();
-// });
