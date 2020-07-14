87483c4d338c7d64db265200de1b19af69b9a886
Ravi Vijay <ravi.vijay@meshdynamics.io>
2020-07-14 10:26:53 +0530

    Feature/improve batch performance (#106)
    * Write to a byte array output stream directly
    
    * Bump Version
    
    * Directly serialize event in entity output stream (save a copy)
    
    * Avoid StringBuilder construction if debug disabled
    
    Co-authored-by: Ravi Vijay <ravi.vijay@cubecorp.io>

--- src/main/java/io/cube/agent/logging/ProxyEventBatchConsumer.java
+++ src/main/java/io/cube/agent/logging/ProxyEventBatchConsumer.java
@@ -1,5 +1,6 @@
 package io.cube.agent.logging;
 
+import java.io.*;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,13 +8,12 @@ import java.util.Optional;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
-import org.apache.http.Consts;
+import io.md.utils.UtilException;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.AbstractHttpEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lmax.disruptor.EventHandler;
 
 import io.cube.agent.CommonConfig;
@@ -36,6 +36,10 @@ public class ProxyEventBatchConsumer {
 
 	private long maxWaitTimeMillis = 5000;
 
+	//long totalTimeInMillis;
+	//long count;
+
+
 	public ProxyEventBatchConsumer() {
 		new Thread(() -> {
 			while (true) {
@@ -56,45 +60,74 @@ public class ProxyEventBatchConsumer {
 		}).start();
 	}
 
+	class MDEventEntity extends AbstractHttpEntity {
+
+		private List<Event> mdEventList;
+		private long contentLength;
+		public MDEventEntity(List<Event> mdEvent) {
+			this.mdEventList = mdEvent;
+		}
+
+		@Override
+		public boolean isRepeatable() {
+			return false;
+		}
+
+		@Override
+		public long getContentLength() {
+			return -1;
+		}
+
+		@Override
+		public InputStream getContent() throws IOException, UnsupportedOperationException {
+			return null;
+		}
+
+		@Override
+		public void writeTo(OutputStream outputStream) throws IOException {
+			mdEventList.forEach(UtilException.rethrowConsumer(mdEvent ->
+			{
+				outputStream.write("{ \"cubeEvent\" : ".getBytes());
+				CubeObjectMapperProvider.getInstance().writeValue(outputStream, mdEvent);
+				outputStream.write(" } ".getBytes());
+				outputStream.write("\n".getBytes());
+				if (LOGGER.isDebugEnabled()) {
+					StringBuilder metaDataStringBuilder = new StringBuilder();
+					if (mdEvent.metaData != null) {
+						mdEvent.metaData.forEach((x, y) ->
+							metaDataStringBuilder.append(x).append("->")
+								.append(y).append(" "));
+					}
+					LOGGER.debug("SERIALIZED EVENT FOR BATCH REQUEST :: SERVICE :: " +
+						mdEvent.service + " :: API PATH :: " + mdEvent.apiPath
+						+ " :: REQ ID :: " + mdEvent.reqId + " :: TRACE ID :: "
+						+ mdEvent.getTraceId() + " :: SPAN ID :: " + mdEvent.spanId
+						+ " :: PARENT SPAN ID " + mdEvent.parentSpanId
+						+ " :: EVENT TYPE :: " + mdEvent.eventType
+						+ " :: EVENT META DATA " + metaDataStringBuilder.toString());
+				}
+			}));
+			outputStream.flush();
+		}
+
+		@Override
+		public boolean isStreaming() {
+			return false;
+		}
+	}
+
 	public void emptyBuffer() {
 		long currentTime = System.currentTimeMillis();
 		if (temporaryBuffer.size() >= maxQueueSize || (currentTime - lastTimeStamp >= 2000
 			&& temporaryBuffer.size()
 			> 0)) {
-			StringBuilder builder = new StringBuilder();
-			temporaryBuffer.forEach(
-				bufferEvent -> {
-					try {
-						builder.append("{ \"cubeEvent\" : ");
-						builder.append(
-							CubeObjectMapperProvider.getInstance()
-								.writeValueAsString(bufferEvent));
-						builder.append(" } ");
-						builder.append("\n");
-						StringBuilder metaDataStringBuilder = new StringBuilder();
-						if (bufferEvent.metaData != null) {
-							bufferEvent.metaData.forEach((x, y) ->
-								metaDataStringBuilder.append(x).append("->")
-									.append(y).append(" "));
-						}
-						LOGGER.info("SERIALIZED EVENT FOR BATCH REQUEST :: SERVICE :: " +
-							bufferEvent.service + " :: API PATH :: " + bufferEvent.apiPath
-							+ " :: REQ ID :: " + bufferEvent.reqId + " :: TRACE ID :: "
-							+ bufferEvent.getTraceId() + " :: SPAN ID :: " + bufferEvent.spanId
-							+ " :: PARENT SPAN ID " + bufferEvent.parentSpanId
-							+ " :: EVENT TYPE :: " + bufferEvent.eventType
-							+ " :: EVENT META DATA " + metaDataStringBuilder.toString());
-					} catch (JsonProcessingException e) {
-						LOGGER.error("Error while converting event to string", e);
-					}
-				}
-			);
-			String entity = builder.toString();
+			List<Event> toWire = new ArrayList<>();
+			toWire.addAll(temporaryBuffer);
+			MDEventEntity eventEntity = new MDEventEntity(toWire);
 			URI recordURI = URI.create(CommonConfig.getInstance().CUBE_RECORD_SERVICE_URI)
 				.resolve("cs/").resolve("storeEventBatch");
 			HttpPost recordReqbuilder = new HttpPost(recordURI);
-			StringEntity stringEntity = new StringEntity(entity, Consts.UTF_8);
-			recordReqbuilder.setEntity(stringEntity);
+			recordReqbuilder.setEntity(eventEntity);
 			recordReqbuilder.setHeader("Content-Type", Constants.APPLICATION_X_NDJSON);
 			Optional<String> response = CubeClient.getResponse(recordReqbuilder);
 			LOGGER.info("Got Response for batch store event request " + response.orElse("NA"));
@@ -106,6 +139,7 @@ public class ProxyEventBatchConsumer {
 	public EventHandler<ValueEvent> getEventHandler() {
 		return (event, sequence, endOfBatch)
 			-> {
+			//long startTime = System.currentTimeMillis();
 			writeLock.lock();
 			try {
 				temporaryBuffer.add(event.getValue());
@@ -114,6 +148,13 @@ public class ProxyEventBatchConsumer {
 				LOGGER.error("Error while converting ");
 			} finally {
 				writeLock.unlock();
+				/*totalTimeInMillis += System.currentTimeMillis() - startTime;
+				count++;
+				if (count % 100 == 0) {
+					System.out.println("Time taken " + totalTimeInMillis*1.0/1000 + " millis for count : " +  count);
+					count = 0;
+					totalTimeInMillis = 0;
+				}*/
 			}
 
 		};
