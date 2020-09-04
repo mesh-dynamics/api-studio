0fb007c7fc53e0559ef3239cfe9255c3f63463e6
abdulquyoombhat <abdulquyoombhat41@gmail.com>
2020-09-04 15:17:43 +0530

    cube-2107-config-apis (#299)

---
+++ src/main/java/com/cubeui/backend/web/rest/ConfigController.java
@@ -0,0 +1,161 @@
+package com.cubeui.backend.web.rest;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.ResponseEntity.created;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.status;
+
+import com.cubeui.backend.domain.App;
+import com.cubeui.backend.domain.Config;
+import com.cubeui.backend.domain.Customer;
+import com.cubeui.backend.domain.DTO.ConfigDTO;
+import com.cubeui.backend.domain.Service;
+import com.cubeui.backend.repository.AppRepository;
+import com.cubeui.backend.repository.ConfigRepository;
+import com.cubeui.backend.repository.CustomerRepository;
+import com.cubeui.backend.repository.ServiceRepository;
+import com.cubeui.backend.security.Validation;
+import com.cubeui.backend.security.jwt.JwtTokenProvider;
+import com.cubeui.backend.web.ErrorResponse;
+import com.cubeui.backend.web.exception.ConfigExistsException;
+import com.cubeui.backend.web.exception.RecordNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/config")
+public class ConfigController {
+
+  @Autowired
+  private ConfigRepository configRepository;
+  @Autowired
+  private Validation validation;
+  @Autowired
+  private CustomerRepository customerRepository;
+  @Autowired
+  private AppRepository appRepository;
+  @Autowired
+  private ServiceRepository serviceRepository;
+  @Autowired
+  private JwtTokenProvider jwtTokenProvider;
+
+  @GetMapping("/get")
+  public ResponseEntity getConfigs(HttpServletRequest request,
+      @RequestParam(value="customer", required = true) String customer,
+      @RequestParam(value="app", required = false) String app,
+      @RequestParam(value="service", required = false) String service,
+      @RequestParam(value="configType", required = false) String configType,
+      @RequestParam(value="key", required = false) String key) {
+    validation.validateCustomerName(request,customer);
+    String userId = jwtTokenProvider.getUser(request).getUsername();
+    Config query = Config.builder().customer(customer).userId(userId)
+        .app(app).service(service)
+        .configType(configType)
+        .key(key).build();
+    List<Config> response = this.configRepository.findAll(Example.of(query));
+    return ok(response);
+  }
+
+  @PostMapping("/insert")
+  public ResponseEntity saveConfig(HttpServletRequest request, @RequestBody ConfigDTO configDTO) {
+    validation.validateCustomerName(request, configDTO.getCustomer());
+    String userId = jwtTokenProvider.getUser(request).getUsername();
+    Optional<Config> existingConfig = this.configRepository
+        .findByKeyAndCustomerAndAppAndConfigTypeAndUserId(configDTO.getKey(), configDTO.getCustomer(),
+            configDTO.getApp(), configDTO.getConfigType(), userId);
+    if(existingConfig.isPresent()) {
+      throw new ConfigExistsException(String.format("key=%s, customer=%s, app=%s, configType=%s",
+          configDTO.getKey(), configDTO.getCustomer(), configDTO.getApp(), configDTO.getConfigType()));
+    }
+    Customer customer = this.customerRepository.findByName(configDTO.getCustomer()).get();
+    if(configDTO.getService() != null && configDTO.getApp() == null) {
+      return status(BAD_REQUEST).body(new ErrorResponse("App name is mandatory if service value is given"));
+    }
+    if(configDTO.getApp() != null) {
+      Optional<App> app = this.appRepository.findByNameAndCustomerId(configDTO.getApp(), customer.getId());
+
+      if(app.isEmpty()) {
+        return status(BAD_REQUEST).body(new ErrorResponse("App with name=" +configDTO.getApp() + " not found" ));
+      }
+
+      if(configDTO.getService() != null) {
+        Optional<Service> service = this.serviceRepository.findByNameAndAppId(configDTO.getService(), app.get().getId());
+
+        if(service.isEmpty()) {
+          return status(BAD_REQUEST).body(new ErrorResponse("Service with name=" +configDTO.getService() + " not found" ));
+        }
+      }
+    }
+    Config saved = this.configRepository.save(
+        Config.builder()
+            .customer(configDTO.getCustomer())
+            .userId(userId)
+            .app(configDTO.getApp())
+            .service(configDTO.getService())
+            .configType(configDTO.getConfigType())
+            .key(configDTO.getKey())
+            .value(configDTO.getValue())
+            .build()
+    );
+    return created(
+        ServletUriComponentsBuilder
+            .fromContextPath(request)
+            .path(request.getServletPath() + "/{id}")
+            .buildAndExpand(saved.getId())
+            .toUri())
+        .body(saved);
+
+  }
+
+  @PostMapping("/delete/{id}")
+  public ResponseEntity delete(HttpServletRequest request, @PathVariable Long id) {
+    String userId = jwtTokenProvider.getUser(request).getUsername();
+    Optional<Config> config = this.configRepository.findByIdAndUserId(id, userId);
+
+    if(config.isPresent()) {
+      this.configRepository.deleteById(id);
+      return ok(String.format("Config deleted for id=%s", id));
+    }else {
+      throw new RecordNotFoundException(
+          String.format("No Config found for id=%s", id));
+    }
+  }
+
+  @PostMapping("/update/{id}")
+  public ResponseEntity update(HttpServletRequest request, @RequestBody ConfigDTO configDTO,
+      @PathVariable Long id) {
+    validation.validateCustomerName(request, configDTO.getCustomer());
+    String userId = this.jwtTokenProvider.getUser(request).getUsername();
+    Optional<Config> existingConfig = this.configRepository.findByIdAndUserId(id, userId);
+    if (existingConfig.isEmpty()) {
+      throw new RecordNotFoundException(String.format("No Config found for id=%s", id));
+    }
+    Optional<Config> configCheck = this.configRepository.findByKeyAndCustomerAndAppAndConfigTypeAndUserId(configDTO.getKey(), configDTO.getCustomer(),
+        configDTO.getApp(), configDTO.getConfigType(), userId);
+    if(configCheck.isPresent()) {
+      throw new ConfigExistsException(String.format(" key=%s, customer=%s, app=%s, configType=%s",
+          configDTO.getKey(), configDTO.getCustomer(), configDTO.getApp(), configDTO.getConfigType()));
+    }
+    Config config = existingConfig.get();
+    config.setService(configDTO.getService());
+    config.setValue(configDTO.getValue());
+    config.setKey(configDTO.getKey());
+    config.setConfigType(configDTO.getConfigType());
+    config.setApp(configDTO.getApp());
+    this.configRepository.save(config);
+    return ok(config);
+  }
+
+}
