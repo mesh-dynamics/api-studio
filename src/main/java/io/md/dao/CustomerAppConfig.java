872cfe8381de19ea90244d5992e0fed50fbd845c
Gaurav Kulshreshtha <gaurav.kulshreshtha@meshdynamics.io>
2020-10-01 15:51:40 +0530

    CustomerAppConfig tracer final and builder

--- src/main/java/io/md/dao/CustomerAppConfig.java
+++ src/main/java/io/md/dao/CustomerAppConfig.java
@@ -1,10 +1,25 @@
 package io.md.dao;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class CustomerAppConfig {
 
-    @JsonProperty("tracer")
-    public String tracer;
+    public final String tracer;
+
+    private CustomerAppConfig(String tracer){
+        this.tracer = tracer;
+    }
+
+    public static class Builder{
+        private String tracer;
+
+        public Builder withTracer(String tracer){
+            this.tracer = tracer;
+            return this;
+        }
+
+        public CustomerAppConfig build(){
+            return new CustomerAppConfig(this.tracer);
+        }
+    }
+
 }
 
