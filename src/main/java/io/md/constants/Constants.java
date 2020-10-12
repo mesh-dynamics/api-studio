6c29c73544995eb5db9f6cf8b7af27bcc12c3b3c
Ravi Vijay <ravi.vijay@cubecorp.io>
2020-10-12 10:01:46 +0530

    Unwrap Wrap working

--- src/main/java/io/md/constants/Constants.java
+++ src/main/java/io/md/constants/Constants.java
@@ -6,6 +6,8 @@ public class Constants {
 	public static final String APPLICATION_X_NDJSON = "application/x-ndjson";
 	public static final String APPLICATION_X_MSGPACK = "application/x-msgpack";
 	public static final String APPLICATION_GRPC = "application/grpc";
+	public static final String IMAGE_JPEG = "image/jpeg";
+	public static final String IMAGE_PNG = "image/png";
 	public static final String DEFAULT_REQUEST_ID = "c-request-id";
 	public static final String DEFAULT_TRACE_FIELD = "x-b3-traceid";
 	public static final String DEFAULT_SPAN_FIELD = "x-b3-spanid";
