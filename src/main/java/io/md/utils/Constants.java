f3d5a1fa172087404bb03df8f69ec3c85ff4aef4
Abdul Quyoom <abdulquyoombhat@gmail.com>
2020-11-05 18:22:02 +0530

    add_constants_diffPathFacets

--- src/main/java/io/md/utils/Constants.java
+++ src/main/java/io/md/utils/Constants.java
@@ -197,6 +197,7 @@ public class Constants {
     public static final String DIFF_RES_FACET = "diffResFacets";
     public static final String SERVICE_FACET = "serviceFacets";
     public static final String PATH_FACET = "pathFacets";
+    public static final String DIFF_PATH_FACET = "diffPathFacets";
     public static final String SAMPLING_FACET = "samplingFacets";
 
 	public static final String REPLAY_TRACE_ID = "replayTraceId";
