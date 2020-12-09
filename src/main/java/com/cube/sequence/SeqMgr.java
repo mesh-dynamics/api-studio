d4bf3af81eb97be2e466ba63d4857bcb0cf4e2fe
Gaurav Kulshreshtha <gaurav.kulshreshtha@meshdynamics.io>
2020-12-09 23:26:35 +0530

    return stream

--- src/main/java/com/cube/sequence/SeqMgr.java
+++ src/main/java/com/cube/sequence/SeqMgr.java
@@ -8,20 +8,21 @@ import java.util.stream.Stream;
 public class SeqMgr {
 
     // assuming the incoming events are in right order sorted order
-    public static void createSeqId(Stream<Event> goldenEvents , long size){
+    public static Stream<Event> createSeqId(Stream<Event> goldenEvents , long size){
 
         SequenceGenerator gen = new SequenceGenerator(size);
 
         Iterator<String> seqItr = gen.iterator();
-        goldenEvents.forEach(e->{
+        return goldenEvents.map(e->{
             String seqId = seqItr.next();
             e.setSeqId(seqId);
+            return e;
         });
 
     }
 
     // assuming the movedEventsBatch are in right sorted order
-    public static void insertBetween(Event insertAfter , Event insertBefore , Stream<Event> movedEventsBatch , int size) {
+    public static Stream<Event> insertBetween(Event insertAfter , Event insertBefore , Stream<Event> movedEventsBatch , int size) {
 
         long prevSeq =  BaseCharUtils.convertToNumber(insertAfter.getSeqId());
         long nextSeq =  BaseCharUtils.convertToNumber(insertBefore.getSeqId());
@@ -41,9 +42,10 @@ public class SeqMgr {
         Iterator<String> seqItr = gen.iterator();
         final String finalBasePadding = basePadding;
         final boolean padding = !finalBasePadding.isEmpty();
-        movedEventsBatch.forEach(e->{
+        return movedEventsBatch.map(e->{
             String seqId = padding ? finalBasePadding + seqItr.next() : seqItr.next();
             e.setSeqId(seqId);
+            return e;
         });
 
     }
