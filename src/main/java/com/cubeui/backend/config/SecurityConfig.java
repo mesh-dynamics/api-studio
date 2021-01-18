c9c39389706089ccd646140bc56531dba43c5467
abdulquyoombhat <abdulquyoombhat41@gmail.com>
2021-01-18 14:58:07 +0530

    populate api for token (#470)
    * populate api for token
    
    * populate api for token
    
    * populate api for token
    
    Co-authored-by: Abdul Quyoom <abdulquyoombhat@gmail.com>

--- src/main/java/com/cubeui/backend/config/SecurityConfig.java
+++ src/main/java/com/cubeui/backend/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/health").permitAll()
                 .antMatchers("/api/login").permitAll()
+                .antMatchers("/api/mst/**").permitAll()
                 .antMatchers("/api/token").permitAll()
                 .antMatchers("/api/config/get").permitAll()
 //                .antMatchers("/api/data").permitAll()
