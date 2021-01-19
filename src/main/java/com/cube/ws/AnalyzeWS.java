a580c626e8a4f5cd8b24e826af327cc630aa4c99
Abdul Quyoom <abdulquyoombhat@gmail.com>
2021-01-19 12:13:30 +0530

    add todo comment

--- src/main/java/com/cube/ws/AnalyzeWS.java
+++ src/main/java/com/cube/ws/AnalyzeWS.java
@@ -1602,6 +1602,7 @@ public class AnalyzeWS {
 	    traceCollectionMap.forEach((traceCollectionKey, events) -> {
 	      List<Event> parentRequestEvents = apiTraceFacetQuery.apiPath.map(path -> {
 	          // get parent events based on apiPath filter if it is non-empty
+					//TODO Use ApiPathRegex
 							String updatedPath = path.replace("*", ".*");
               return events.stream()
                   .filter(e -> e.apiPath.matches(updatedPath) && apiTraceFacetQuery.service.map(e.service::equals).orElse(true))
