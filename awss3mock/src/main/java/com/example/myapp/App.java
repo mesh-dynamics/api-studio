4ae927824a824575caf55d00d2432b43e662269a
nmani1978 <muthumani@gmail.com>
2021-03-26 17:47:05 +0530

    Add files via upload

---
+++ awss3mock/src/main/java/com/example/myapp/App.java
@@ -0,0 +1,47 @@
+package com.example.myapp;
+
+import java.io.IOException;
+import java.net.URI;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.apache.ProxyConfiguration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        SdkHttpClient apacheClientFactory =
+            ApacheHttpClient.builder()
+                .proxyConfiguration(ProxyConfiguration.builder()
+                    .endpoint(URI.create("http://127.0.0.1:9002")).build()).build();
+
+
+        Region region = Region.US_WEST_2;
+        S3Client s3 = S3Client.builder()
+            .httpClient(apacheClientFactory).region(region).build();
+
+        //S3Client s3 = S3Client.builder().region(region).build();
+
+        String bucket = "bucketmdtest";
+        String key = "key";
+
+        System.out.println("Uploading object...");
+
+        s3.putObject(PutObjectRequest.builder().bucket(bucket).key(key)
+                        .build(),
+                RequestBody.fromString("Testing with the AWS SDK for Java"));
+
+        System.out.println("Upload complete");
+        System.out.printf("%n");
+
+        System.out.println("Closing the connection to Amazon S3");
+        s3.close();
+        System.out.println("Connection closed");
+        System.out.println("Exiting...");
+    }
+}
