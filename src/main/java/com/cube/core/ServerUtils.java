fa94c8335729e5803d67c5154354102bb3f00f41
Ravi Vijay <ravi.vijay@meshdynamics.io>
2021-03-31 14:13:20 +0530

    Bug FIx

--- src/main/java/com/cube/core/ServerUtils.java
+++ src/main/java/com/cube/core/ServerUtils.java
@@ -431,9 +431,14 @@ public class ServerUtils {
         Optional<String> templateSetLabel, Optional<String> name, Optional<String> userId
         , Instant timeStamp, String labelValue, RecordingType type) {
         String collection = UUID.randomUUID().toString();
-        String templateSetNameFinal = templateSetName.orElse(recording.templateSetName);
-        String templateSetLabelFinal = templateSetLabel.orElse(
-            recording.templateSetLabel);
+        String templateSetNameFinal = templateSetName.or(() -> (recording.templateSetName != null &&
+            (!recording.templateSetName.isEmpty())) ? Optional.of(recording.templateSetName)
+            : Optional.empty()).orElse(Utils.extractTemplateSetNameAndLabel(
+                recording.templateVersion).getLeft());
+        String templateSetLabelFinal = templateSetLabel.or(() -> (recording.templateSetLabel != null &&
+            (!recording.templateSetLabel.isEmpty())) ? Optional.of(recording.templateSetLabel)
+            : Optional.empty()).orElse(Utils.extractTemplateSetNameAndLabel(
+            recording.templateVersion).getRight());
         String templateSetVersion = Utils.createTemplateSetVersion(templateSetNameFinal,
             templateSetLabelFinal);
         RecordingBuilder recordingBuilder = new RecordingBuilder(
