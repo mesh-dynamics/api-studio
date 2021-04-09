938dfaae47a4055380191b99cfb5123a0a7e8a9a
Gaurav Kulshreshtha <gaurav.kulshreshtha@meshdynamics.io>
2021-04-09 08:49:59 +0530

    pubsub mgr jedis connection reuse

--- src/main/java/com/cube/pubsub/PubSubMgr.java
+++ src/main/java/com/cube/pubsub/PubSubMgr.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.md.cache.Constants.PubSubContext;
 import io.md.utils.CubeObjectMapperProvider;
+import redis.clients.jedis.Jedis;
 
 import com.cube.ws.Config.JedisConnResourceProvider;
 
@@ -17,10 +18,13 @@ public class PubSubMgr {
 
 	Logger LOGGER = LogManager.getLogger(PubSubMgr.class);
 	private final JedisConnResourceProvider jedisPool;
+	private Jedis jedis;
 	public final ObjectMapper jsonMapper = CubeObjectMapperProvider.getInstance();
 
 	public PubSubMgr(JedisConnResourceProvider jedisPool){
+
 		this.jedisPool = jedisPool;
+		this.jedis = jedisPool.getResource();
 	}
 
 	public Long publish(PubSubContext context , Map data){
@@ -28,7 +32,7 @@ public class PubSubMgr {
 		try {
 			String strMsg =  jsonMapper.writeValueAsString(new ChannelMsg(context , data));
 			LOGGER.info("Publishing Msg "+strMsg);
-			return jedisPool.getResource().publish(PubSubChannel.MD_PUBSUB_CHANNEL_NAME, strMsg);
+			return jedis.publish(PubSubChannel.MD_PUBSUB_CHANNEL_NAME, strMsg);
 		} catch (JsonProcessingException e) {
 			LOGGER.error("Publish Msg Json Serialization error "+e.getMessage() , e);
 			return -1L;
