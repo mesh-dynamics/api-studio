23fbb45d4a30e3e16030aa0db96048eb5fa318f1
Vishnu Gupta <vishnu.gupta@eshdynamics.io>
2021-06-09 15:32:17 +0530

    Removed unused code

--- ui/src/common/_unused/shareable_link/index.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react'
-// import { Route } from 'react-router'
-// import ShareableLink from "./ShareableLink";
-
-// export default [
-//     <Route exact key="ShareableLink" path="/shareable_link" component={ShareableLink} />,
-// ]
