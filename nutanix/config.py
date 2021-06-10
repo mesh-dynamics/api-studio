5dff9bb740ff5a68f6c2caf764df1f746d4057e3
Ravi Vijay <ravi.vijay@meshdynamics.io>
2021-06-10 09:04:55 +0530

    Moved MobileIron dir in customer_deployment repository master branch to interceptors/envoy dir

--- nutanix/config.py
+++
@@ -1,6 +0,0 @@
-auth_token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNZXNoREFnZW50VXNlckBudXRhbml4LmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ0eXBlIjoicGF0IiwiY3VzdG9tZXJfaWQiOjEyMzMsImlhdCI6MTYwMjA1ODczOSwiZXhwIjoxOTE3NDE4NzM5fQ.C1q5Mrzc1L-JLk15Nf8tIfWUvbPCEPJmAT4aj_NsIkA"
-customer = "Nutanix"
-cube_application = "NutanixApp"
-cube_instanceid = "test"
-run_type = "Record"
-recording_type = "Golden"
