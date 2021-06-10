87661f554825a16fb6bb2472d87ae5b6fcd507d5
Vishnu Gupta <vishnu.gupta@eshdynamics.io>
2021-06-10 15:14:11 +0530

    Auto login feature for localhost

--- ui/src/common/components/Authentication/LoginPage.js
+++ ui/src/common/components/Authentication/LoginPage.js
@@ -1,56 +1,71 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import ResetLink from "./ResetLink";
 import ResetPassword from "./ResetPassword";
-import ResendActivation from './ResendActivation'
+import ResendActivation from "./ResendActivation";
 import "./Login.css";
-import MDCircleLogo from '../../../../public/assets/images/md-circle-logo.png';
+import MDCircleLogo from "../../../../public/assets/images/md-circle-logo.png";
+import authActions from "../../actions/auth.actions";
 
-const LoginPage = (props) => {
-    const { authentication: { loggedIn }} = props;
-    return (
-        <div>
-            {
-                loggedIn ? <Redirect to="/" /> :
-                    (
-                        <div className="flex">
-                            <div className="login-widget">
-                                <div className="row vertical-align-middle">
-                                    <div className="col-md-6 logo-wrapper">
-                                        <div>
-                                            <img src={MDCircleLogo} alt="MD LOGO" />
-                                            <span className="comp-name">Mesh Dynamics</span>
-                                        </div>
-                                        <div className="note">
-                                            This is a Restricted Access beta. Read our Disclaimer for limitations
-                                        </div>
-                                        </div>
-                                        <div className="col-md-6 sign-in-wrapper">
-                                                <Switch>
-                                                    <Route exact path="/login" component={SignIn} />
-                                                    <Route path="/login/register" component={SignUp} />
-                                                    <Route path="/login/reset-link" component={ResetLink} />
-                                                    <Route path="/login/resend-activation-link" component={ResendActivation} />
-                                                    <Route path="/login/reset-password" component={ResetPassword} />
-                                                    <Route path="/login/*" component={SignIn} />
-                                                </Switch>
-                                        </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-            }
+const LoginPage = function (props) {
+  const {
+    authentication: { loggedIn },
+    dispatch,
+  } = props;
+  const isLocalhost = config.apiBaseUrl.includes("//localhost");
+  useEffect(() => {
+    if (isLocalhost && !loggedIn) {
+      dispatch(authActions.localhostLogin());
+    }
+  }, [loggedIn]);
+  return (
+    <div>
+      {loggedIn ? (
+        <Redirect to="/" />
+      ) : (
+        <div className="flex">
+          <div className="login-widget">
+            <div className="row vertical-align-middle">
+              <div className="col-md-6 logo-wrapper">
+                <div>
+                  <img src={MDCircleLogo} alt="MD LOGO" />
+                  <span className="comp-name">Mesh Dynamics</span>
+                </div>
+                <div className="note">This is a Restricted Access beta. Read our Disclaimer for limitations</div>
+              </div>
+              <div className="col-md-6 sign-in-wrapper">
+                {isLocalhost ? (
+                  <>
+                    <div style={{ width: "100%", fontSize: "15px" }}>
+                      Trying to login in local mode. <i className="fa fa-spinner fa-spin" />
+                    </div>
+                  </>
+                ) : (
+                  <Switch>
+                    <Route exact path="/login" component={SignIn} />
+                    <Route path="/login/register" component={SignUp} />
+                    <Route path="/login/reset-link" component={ResetLink} />
+                    <Route path="/login/resend-activation-link" component={ResendActivation} />
+                    <Route path="/login/reset-password" component={ResetPassword} />
+                    <Route path="/login/*" component={SignIn} />
+                  </Switch>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    authentication: state.authentication,
+  authentication: state.authentication,
 });
 
 const connectedLoginPage = connect(mapStateToProps)(LoginPage);
-export { connectedLoginPage as LoginPage};
+export { connectedLoginPage as LoginPage };
--- ui/src/pages/settings/components/DomainSettings.js
+++ ui/src/pages/settings/components/DomainSettings.js
@@ -1,50 +1,38 @@
-import React from 'react';
-import isUrl from 'validator/lib/isURL';
+import React from "react";
+import isUrl from "validator/lib/isURL";
 
-const  DomainSettings = (props) => {
-    const { domain, isSaveButtonDisabled, handleDomainInputChange, handleSaveDomainClick } = props;
+const DomainSettings = (props) => {
+  const { domain, isSaveButtonDisabled, handleDomainInputChange, handleSaveDomainClick } = props;
 
-    return (
-        <div className="col-md-12 col-sm-12 col-xs-12">
-            <div className="panel panel-default">
-                <div className="settings-panel">
-                    <i className="icon-calendar"></i>
-                    <span>Target Domain Settings</span>
-                </div>
-
-                <div className="panel-body">
-                    <br />
-                    <div className="input-group input-group-sm">
-                        <span className="input-group-addon settings-no-border">
-                            <i className="fa fa-globe" aria-hidden="true"></i>
-                        </span>
-                        <input 
-                            id="domain-input" 
-                            type="text" className="form-control settings-no-border"
-                            placeholder="http://example.com"
-                            value={domain}
-                            onChange={handleDomainInputChange}
-                        />
-                    </div>
-                    {!isUrl(domain) && <span className="settings-error-text">The url is not valid</span>}
-                    <br />
-                    <div className="settings-action-buttons">
-                        <span className="settings-margin-left-10">
-                            <button 
-                                id="save-button" 
-                                type="button" 
-                                onClick={handleSaveDomainClick}
-                                disabled={isSaveButtonDisabled}
-                                className="btn btn-sm cube-btn text-center"
-                            >
-                                Save
-                            </button>
-                        </span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="col-md-12 col-sm-12 col-xs-12">
+      <div className="panel panel-default">
+        <div className="settings-panel">
+          <i className="icon-calendar"></i>
+          <span>Target Domain Settings</span>
         </div>
-    );
-}
- 
+
+        <div className="panel-body">
+          <br />
+          <div className="input-group input-group-sm">
+            <span className="input-group-addon settings-no-border">
+              <i className="fa fa-globe" aria-hidden="true"></i>
+            </span>
+            <input id="domain-input" type="text" className="form-control settings-no-border" placeholder="http://example.com" value={domain} onChange={handleDomainInputChange} />
+          </div>
+          {!isUrl(domain, { require_tld: false }) && <span className="settings-error-text">The url is not valid</span>}
+          <br />
+          <div className="settings-action-buttons">
+            <span className="settings-margin-left-10">
+              <button id="save-button" type="button" onClick={handleSaveDomainClick} disabled={isSaveButtonDisabled} className="btn btn-sm cube-btn text-center">
+                Save
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default DomainSettings;
