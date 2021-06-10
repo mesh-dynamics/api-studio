3d725386c0916ba9d44c5180b44915150a7112b9
Ravi Vijay <ravi.vijay@meshdynamics.io>
2021-06-10 09:33:49 +0530

    Merge file envoy-record-cs.j2 deploy_sample_apps/develop into dir interceptors/envoy

--- apps/cube/scripts/update_deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-update_deployment_app() {
-  echo Updating deployment
-	TAG=$1; shift
-	kubectl -n $NAMESPACE set image deployment/cubews-mock-v1 cubews=cubeiocorp/cubews:$TAG
-	kubectl -n $NAMESPACE set image deployment/cubews-record-v1 cubews=cubeiocorp/cubews:$TAG
-	kubectl -n $NAMESPACE set image deployment/cubews-replay-v1 cubews=cubeiocorp/cubews:$TAG
-}
--- apps/moviebook/scripts/update_deployment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-update_deployment_app() {
-  echo Updating deployment
-	TAG=$1; shift
-	kubectl -n $NAMESPACE set image deployment/movieinfo-v1 movieinfo=cubeiocorp/sample_apps-mirest:$TAG
-	kubectl -n $NAMESPACE set image deployment/movieinfo-v2 movieinfo=cubeiocorp/sample_apps-mirest:$TAG
-	kubectl -n $NAMESPACE set image deployment/restwrapjdbc-v1 restwrapjdbc=cubeiocorp/restwrapjdbc:$TAG
-}
