481b0c9cf563453c63f07d796766d01d27e8ea2b
Vishesh Garg <gargvishesh@gmail.com>
2021-06-16 14:55:24 +0530

    build changes for combined repo

---
+++ build.sh
@@ -0,0 +1,44 @@
+RELEASE=1.0
+STANDALONE_GATEWAY_JAR_PATH=services/gateway-standalone/target/gateway-standalone-$RELEASE.jar
+STANDALONE_CORE_JAR_PATH=services/core-standalone/target/core-standalone-$RELEASE.jar
+UI_BIN_PATH=ui/bin
+DEPLOY=false
+
+function usage() {
+    echo "USAGE: $0 <OPTIONS>"
+    echo "OPTIONS:"
+    echo "-h|--help             Usage help"
+    echo "--deploy              Push built artifacts to github"
+    echo "-r|release=<version>  Version of the release (DEFAULT 1.0)"
+    exit 1
+}
+
+for i in "$@"
+do
+case $i in
+    -h|--help)
+    usage
+    shift
+    ;;
+    --deploy)
+    DEPLOY=true
+    shift
+    ;;
+    -r=*|--release=*)
+    RELEASE="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+    echo "Invalid Option"
+    usage
+    ;;
+esac
+done
+
+cp $STANDALONE_GATEWAY_JAR_PATH $UI_BIN_PATH
+cp $STANDALONE_CORE_JAR_PATH $UI_BIN_PATH
+if [ "$DEPLOY" = true ] ; then
+    mvn package -Drevision=$RELEASE -DskipTests
+else mvn deploy -Drevision=$RELEASE -DskipTests
+fi
+
