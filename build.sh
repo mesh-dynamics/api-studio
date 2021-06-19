5b3584a7ba6492014c010ff72a9630abdf0efc82
Vishesh Garg <gargvishesh@gmail.com>
2021-06-19 19:09:22 +0530

    Skip docker build for non-linux build

--- build.sh
+++ build.sh
@@ -1,20 +1,16 @@
 RELEASE=1.0
-DEPLOY=false
+PHASE=package
+BUILD_DOCKER="-Ddockerfile.skip=true"
 
 function usage() {
     echo "USAGE: $0 <OPTIONS>"
     echo "OPTIONS:"
     echo "-h|--help             Usage help"
     echo "--deploy              Push built artifacts to github (DEFAULT package)"
+    echo "--buildDocker         Build docker images (DEFAULT jars-only)"
     echo "-r|release=<version>  Version of the release (DEFAULT 1.0)"
     exit 1
 }
-
-STANDALONE_GATEWAY_SOURCE_JAR_PATH=services/gateway/target/gateway-standalone-$RELEASE.war
-STANDALONE_GATEWAY_TARGET_JAR_PATH=ui/bin/gateway-standalone.jar
-
-STANDALONE_CORE_SOURCE_JAR_PATH=services/core/target/core-standalone-$RELEASE.war
-STANDALONE_CORE_TARGET_JAR_PATH=ui/bin/core-standalone.jar
 
 for i in "$@"
 do
@@ -24,7 +20,11 @@ case $i in
     shift
     ;;
     --deploy)
-    DEPLOY=true
+    PHASE=deploy
+    shift
+    ;;
+    --buildDocker)
+    BUILD_DOCKER=
     shift
     ;;
     -r=*|--release=*)
@@ -38,10 +38,13 @@ case $i in
 esac
 done
 
-if [ "$DEPLOY" = true ] ; then
-    mvn deploy -Drevision=$RELEASE -DskipTests
-else mvn package -Drevision=$RELEASE -DskipTests
-fi
+STANDALONE_GATEWAY_SOURCE_JAR_PATH=services/gateway/target/gateway-$RELEASE.war
+STANDALONE_GATEWAY_TARGET_JAR_PATH=ui/bin/gateway-standalone.jar
+
+STANDALONE_CORE_SOURCE_JAR_PATH=services/core/target/core-$RELEASE.war
+STANDALONE_CORE_TARGET_JAR_PATH=ui/bin/core-standalone.jar
+
+mvn $PHASE -Drevision="$RELEASE" -DskipTests "$BUILD_DOCKER"
 
 cp $STANDALONE_GATEWAY_SOURCE_JAR_PATH $STANDALONE_GATEWAY_TARGET_JAR_PATH
 cp $STANDALONE_CORE_SOURCE_JAR_PATH $STANDALONE_CORE_TARGET_JAR_PATH
